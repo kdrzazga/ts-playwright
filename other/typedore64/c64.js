9ba5338cca922d6020d91e09bba605e9412560e1
kdrzazga <kdrzazga@luxoft.com>
2024-05-24 09:13:36 +0200

    typedore-3.1

--- other/typedore64/c64.js
+++ other/typedore64/c64.js
@@ -1,14 +1,14 @@
 var Commodore64 = /** @class */ (function () {
     function Commodore64() {
         this.tableContent = [
-            "    **** COMMODORE 64 BASIC V2 ****    ",
-            "",
-            " 64K RAM SYSTEM  38911 BASIC BYTES FREE ",
-            "",
+            "<center>    &nbsp**** COMMODORE 64 BASIC V2 ****&nbsp    </center>",
+            "&nbsp",
+            "<center> &nbsp64K RAM SYSTEM  38911 BASIC BYTES FREE&nbsp </center>",
+            "&nbsp",
             "READY."
         ];
-        for (var i = 5; i < 40; i++) {
-            this.tableContent.push("");
+        for (var i = 5; i < 20; i++) {
+            this.tableContent.push("&nbsp");
         }
     }
     Commodore64.prototype.generateHtml = function () {
@@ -20,9 +20,22 @@ var Commodore64 = /** @class */ (function () {
         html.push("</table>");
         return html.join("");
     };
+    Commodore64.prototype.generateBorder = function () {
+        var html = [];
+        html.push("<table bgcolor=\"#6060c0\" width=\"100%\">");
+        for (var i = 0; i < 3; i++) {
+            html.push("<tr><td width=\"10%\">&nbsp</td><td width=\"80%\">&nbsp</td><td width=\"10%\">&nbsp</td></tr>");
+        }
+        html.push("</table>");
+        return html.join("");
+    };
     return Commodore64;
 }());
 var commodore64 = new Commodore64();
 var html = commodore64.generateHtml();
 var div = document.getElementById('commodore64');
+var topBorderDiv = document.getElementById('top-border');
+var bottomBorderDiv = document.getElementById('bottom-border');
 div.innerHTML = html;
+topBorderDiv.innerHTML = commodore64.generateBorder();
+bottomBorderDiv.innerHTML = commodore64.generateBorder();
