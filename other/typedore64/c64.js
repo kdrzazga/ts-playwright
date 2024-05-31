cdcc4d66655d307c92b1ab687ada73058f853ac2
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-05-31 13:36:47 +0200

    Typedore5 (#17)

--- other/typedore64/c64.js
+++ other/typedore64/c64.js
@@ -13,11 +13,18 @@ var Commodore64 = /** @class */ (function () {
             this.tableContent.push("&nbsp");
         }
         this.lastRenderTime = 0;
+        this.canvas = document.createElement('canvas');
+        this.canvas.style.position = 'relative';
+        this.canvas.style.top = '-1px';
+        this.canvas.style.left = '-14px';
+        this.canvas.width = 17;
+        this.canvas.height = 17;
+        this.ctx = this.canvas.getContext('2d');
     }
     Commodore64.prototype.generateHtml = function () {
         var html = [];
         var number = 0;
-        html.push("<table id=\"main\" bgcolor=\"#200080\">");
+        html.push("<table id=\"main\" bgcolor=\"" + Commodore64.BLUE + "\">");
         this.tableContent.forEach(function (line) {
             var strNumber = String(number);
             html.push("<tr><td id=\"row" + strNumber + "\">" + line + "</td></tr>");
@@ -28,18 +35,30 @@ var Commodore64 = /** @class */ (function () {
     };
     Commodore64.prototype.generateBorder = function () {
         var html = [];
-        html.push("<table bgcolor=\"#6060c0\" width=\"100%\">");
+        html.push("<table bgcolor=\"" + Commodore64.LIGHTBLUE + "\" width=\"100%\">");
         for (var i = 0; i < 3; i++) {
             html.push("<tr><td width=\"10%\">&nbsp</td><td width=\"80%\">&nbsp</td><td width=\"10%\">&nbsp</td></tr>");
         }
         html.push("</table>");
         return html.join("");
     };
+    Commodore64.prototype.initBlinker = function () {
+        this.canvasContainer = document.getElementById('row6');
+        this.canvasContainer.appendChild(this.canvas);
+    };
     Commodore64.prototype.blinker = function () {
         var _this = this;
         var currentTime = performance.now();
         var timeSinceLastRender = currentTime - this.lastRenderTime;
-        if (timeSinceLastRender > 1000 / Commodore64.FPS) {
+        if (timeSinceLastRender >= 1000 / Commodore64.FPS) {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            if (this.blink) {
+                this.ctx.fillStyle = Commodore64.LIGHTBLUE;
+            }
+            else {
+                this.ctx.fillStyle = Commodore64.BLUE;
+            }
+            this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
             this.blink = !this.blink;
             this.lastRenderTime = currentTime;
         }
@@ -47,6 +66,8 @@ var Commodore64 = /** @class */ (function () {
         requestAnimationFrame(function () { return _this.blinker(); });
     };
     Commodore64.FPS = 2;
+    Commodore64.BLUE = "#200080";
+    Commodore64.LIGHTBLUE = "#6060c0";
     return Commodore64;
 }());
 var commodore64 = new Commodore64();
@@ -57,4 +78,5 @@ var bottomBorderDiv = document.getElementById('bottom-border');
 div.innerHTML = html;
 topBorderDiv.innerHTML = commodore64.generateBorder();
 bottomBorderDiv.innerHTML = commodore64.generateBorder();
+commodore64.initBlinker();
 commodore64.blinker();
