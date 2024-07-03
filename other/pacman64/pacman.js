54fc8b8255ba6b7f872268e5ac9c57bec041dd7d
jerzy <jerzy.z.bialowiezy@proton.me>
2024-07-03 21:56:50 +0200

    petscii2_pacman1

---
+++ other/pacman64/pacman.js
@@ -0,0 +1,60 @@
+var BOARD_WIDTH = 16;
+var BOARD_HEIGHT = 4;
+var TileType;
+(function (TileType) {
+    TileType[TileType["WALL"] = 0] = "WALL";
+    TileType[TileType["PATH"] = 1] = "PATH";
+})(TileType || (TileType = {}));
+var Tile = /** @class */ (function () {
+    function Tile(type, hasPill) {
+        this.type = type;
+        if (type == TileType.WALL) {
+            this.filePath = 'resources/wall.png';
+        }
+        else if (type == TileType.PATH) {
+            this.filePath = 'resources/path.png';
+        }
+        else
+            this.filePath = '';
+        this.hasPill = hasPill;
+    }
+    return Tile;
+}());
+function generateBoard(boardString) {
+    var board = [];
+    for (var i = 0; i < BOARD_HEIGHT; i++) {
+        board[i] = [];
+        for (var j = 0; j < BOARD_WIDTH; j++) {
+            var char = boardString[i * 2 + j];
+            if (char == 'w') {
+                board[i].push(new Tile(TileType.WALL, false));
+            }
+            else if (char == 'p') {
+                board[i].push(new Tile(TileType.PATH, true));
+            }
+        }
+    }
+    return board;
+}
+function drawBoard(board) {
+    var html = '<table>';
+    for (var i = 0; i < BOARD_HEIGHT; i++) {
+        html += '<tr>';
+        for (var j = 0; j < BOARD_WIDTH; j++) {
+            html += '<td>';
+            html += '</td>';
+        }
+        html += '</tr>';
+    }
+    html += '</table>';
+    var boardDiv = document.getElementById('board');
+    boardDiv.innerHTML = html;
+}
+var boardString = "wwwwwwwwwwwwwwww" +
+    "wpppwpppppwpppww" +
+    "wpwpwpwpwpwpwpww" +
+    "wpppwpppppwpppww" +
+    "";
+var board = generateBoard(boardString);
+drawBoard(board);
+console.log(board);
---
+++ other/pacman64/pacman.ts
@@ -0,0 +1,70 @@
+const BOARD_WIDTH = 16;
+const BOARD_HEIGHT = 4;
+
+enum TileType {
+  WALL,
+  PATH
+}
+
+class Tile {
+  type: TileType;
+  filePath : string;
+  hasPill: boolean;
+
+  constructor(type: TileType, hasPill: boolean) {
+    this.type = type;
+	if (type == TileType.WALL){
+		this.filePath = 'resources/wall.png';
+	}
+	else if (type == TileType.PATH){
+		this.filePath = 'resources/path.png';
+	}
+	else this.filePath = '';
+	
+    this.hasPill = hasPill;
+  }
+}
+
+function generateBoard(boardString: string): Tile[][] {
+  const board: Tile[][] = [];
+
+  for (let i = 0; i < BOARD_HEIGHT; i++) {
+    board[i] = [];
+    for (let j = 0; j < BOARD_WIDTH; j++) {
+      const char = boardString[i * 2 + j];
+      if (char == 'w') {
+        board[i].push(new Tile(TileType.WALL, false));
+      } else if (char == 'p') {
+        board[i].push(new Tile(TileType.PATH, true));
+      }
+    }
+  }
+
+  return board;
+}
+
+function drawBoard(board){
+	 let html = '<table>';
+	 for (let i = 0; i < BOARD_HEIGHT; i++) {
+		html += '<tr>'
+		for (let j = 0; j < BOARD_WIDTH; j++) {
+			html += '<td>'
+			html += '</td>'
+		}
+		html += '</tr>'
+	 }
+	 html += '</table>';
+	 let boardDiv = document.getElementById('board');
+	 boardDiv.innerHTML = html;
+}
+
+const boardString = 
+"wwwwwwwwwwwwwwww" +
+"wpppwpppppwpppww" +
+"wpwpwpwpwpwpwpww" +
+"wpppwpppppwpppww" +
+"";
+const board = generateBoard(boardString);
+drawBoard(board);
+
+console.log(board);
