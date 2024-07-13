20173f2951e5528df6a0b12fb16a8de186c521f5
kdrzazga <kdrzazga@luxoft.com>
2024-07-13 21:26:06 +0200

    bruce-lee

---
+++ other/c64frames_res/frames.js
@@ -0,0 +1,33 @@
+var counter = 3;
+var counterMax = 3;
+function moveBruceLee() {
+    var bruce = document.getElementById('bruce');
+    bruce.style.left = (parseInt(bruce.style.left) - 40) + 'px';
+    console.log("Bruce Lee position = ", bruce.style.left);
+}
+function conditionalReset() {
+    var bruce = document.getElementById('bruce');
+    if (parseInt(bruce.style.left) < -10000) {
+        bruce.style.left = '2677px';
+    }
+}
+function timeLoop() {
+    var _this = this;
+    var lastRenderTime = 0;
+    console.log("time loop");
+    var currentTime = performance.now();
+    var timeSinceLastRender = currentTime - lastRenderTime;
+    if (timeSinceLastRender >= 15) {
+        if (counter < 0) {
+            moveBruceLee();
+            conditionalReset();
+            counter = counterMax;
+            lastRenderTime = currentTime;
+        }
+        else {
+            counter--;
+        }
+    }
+    requestAnimationFrame(function () { return _this.timeLoop(); });
+}
+timeLoop();
