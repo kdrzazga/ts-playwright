0fc0f1a52e2f6ccfef7b9938fc546a5c3a09d602
jerzy <jerzy.z.bialowiezy@proton.me>
2024-07-19 14:04:28 +0200

    more-tunes

--- other/skull64/audio.js
+++ other/skull64/audio.js
@@ -1,24 +1,30 @@
 function play() {
   var audioArray = [
-	'resources/supremacy.mp3',
-	'resources/wilderness.mp3',
-    '../commando64/resources/commando.mp3',
-    '../ik64/resources/ik.mp3'
-  ];
+	['resources/supremacy.mp3', (4*60+10)*1000],
+	['resources/LN2.mp3', (4*60+40)*1000],
+	['resources/wilderness.mp3', (4*60+40)*1000],
+    ['../commando64/resources/commando.mp3', (8*60+40)*1000],
+    ['resources/natoAC.mp3', (3*60+15)*1000],
+    ['resources/LN1wasteland.mp3', (4*60+0)*1000],
+    ['../ik64/resources/ik.mp3', (10*60+50)*1000],
+    ['resources/BB.mp3', (1*60+20)*1000],
+    ['resources/mikie.mp3', (1*60+20)*1000]
+  ]; //over 40 min of music
 
   var currentAudio = 0;
   var audio = new Audio(audioArray[currentAudio]);
 
   function playAudio() {
-    audio.play();
     currentAudio = (currentAudio + 1) % audioArray.length;
-    audio.src = audioArray[currentAudio];
+    audio.src = audioArray[currentAudio][0];
     audio.play();
+	const now = Date.now();
+	const formattedTimestamp = new Date(now).toISOString();
+	console.log(`${formattedTimestamp} Music started - ${audioArray[currentAudio][0]} duration: ${audioArray[currentAudio][1]}`);
   }
 
   playAudio();
-  setInterval(playAudio, (10*60+40)*1000);
-  console.log('Music started.');
+  setInterval(playAudio, audioArray[currentAudio][1]);  
 }
 
 
