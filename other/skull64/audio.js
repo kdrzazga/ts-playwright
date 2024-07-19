57227933ce31396c4233b53e7893cae9673ee7a7
kdrzazga <kdrzazga@luxoft.com>
2024-07-19 10:34:22 +0200

    loop1

--- other/skull64/audio.js
+++ other/skull64/audio.js
@@ -1,11 +1,26 @@
+var audio = new Audio('../commando64/resources/commando.mp3');
+
 function play() {
-  var audio = new Audio('resources/commando.mp3');
-  audio.play();
-  setInterval(function() {
+  var audioArray = [
+    '../commando64/resources/commando.mp3',
+    '../ik64/resources/ik.mp3',
+  ];
+
+  var currentAudio = 0;
+  var audio = new Audio(audioArray[currentAudio]);
+
+  function playAudio() {
     audio.play();
-  }, 8130);
+    currentAudio = (currentAudio + 1) % audioArray.length;
+    audio.src = audioArray[currentAudio];
+    audio.play();
+  }
+
+  playAudio();
+  setInterval(playAudio, (10*60+40)*1000);
   console.log('Music started.');
 }
+
 
 function hide(){
 	var button = document.getElementById('start-tune');
