b0fe703ca4692bcc8746cd61f44cfae67e490fc4
kdrzazga <kdrzazga@luxoft.com>
2024-08-02 15:21:54 +0200

    ik-cover-tweak

--- other/2screen-ik/ik.js
+++ other/2screen-ik/ik.js
@@ -1,10 +1,25 @@
-var x = 40;
+var fps = 1;
+var x = 260;
+var lastRenderTime = 0;
+var speed = 15;
 function moveKarateka() {
     var karateka = document.getElementById('karateka');
-    karateka.style.left = new String(x) + 'px';
-    x++;
-    requestAnimationFrame(function () { return moveKarateka(); });
+    karateka.style.left = "".concat(x, "px");
+    x += speed;
     console.log('' + karateka.style.left);
 }
+function ikpostToFront() {
+    var ikpost = document.getElementById('ik-cover');
+    ikpost.style.zIndex = '3';
+}
 console.log('hello');
-moveKarateka();
+function animate(currentTime) {
+    var timeSinceLastRender = currentTime - lastRenderTime;
+    if (timeSinceLastRender > 100 / fps) {
+        moveKarateka();
+        ikpostToFront();
+        lastRenderTime = currentTime;
+    }
+    requestAnimationFrame(animate);
+}
+requestAnimationFrame(animate);
--- other/2screen-ik/ik.ts
+++ other/2screen-ik/ik.ts
@@ -1,14 +1,33 @@
-var x = 40;
+const fps = 1;
+var x = 260;
+let lastRenderTime = 0;
+var speed = 15;
 
 function moveKarateka(){
 	
 	var karateka = document.getElementById('karateka');
-	karateka.style.left = new String(x) + 'px';
-	x++;
-	requestAnimationFrame(() => moveKarateka());
+	karateka.style.left = `${x}px`;
+    x+=speed;
 	console.log('' + karateka.style.left);
+}
+
+function ikpostToFront(){
+	var ikpost = document.getElementById('ik-cover');
+	ikpost.style.zIndex = '3';
 }
 
 console.log('hello');
 
-moveKarateka();
+
+function animate(currentTime) {
+    const timeSinceLastRender = currentTime - lastRenderTime;
+
+    if (timeSinceLastRender > 100 / fps) {
+		moveKarateka();
+		ikpostToFront();
+		lastRenderTime = currentTime;
+    }
+    requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
