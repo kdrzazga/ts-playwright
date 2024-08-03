bc7096949dadc6b973bb9260f9cb3c06a685d19c
kdrzazga <kdrzazga@luxoft.com>
2024-08-03 17:53:29 +0200

    walk1

--- other/2screen-ik/ik.js
+++ other/2screen-ik/ik.js
@@ -1,25 +1,72 @@
 var fps = 1;
-var x = 260;
 var lastRenderTime = 0;
-var speed = 15;
-function moveKarateka() {
-    var karateka = document.getElementById('karateka');
-    karateka.style.left = "".concat(x, "px");
-    x += speed;
-    console.log('' + karateka.style.left);
-}
+var CircularList = /** @class */ (function () {
+    function CircularList(list) {
+        this.list = list;
+        this.index = 0;
+    }
+    CircularList.prototype.next = function () {
+        var result = this.list[this.index];
+        this.index = (this.index + 1) % this.list.length;
+        return result;
+    };
+    return CircularList;
+}());
+var Karateka = /** @class */ (function () {
+    function Karateka() {
+        this.speed = 15;
+        this.x = 260;
+        this.y = 260;
+        this.picture = Karateka.pictureRight;
+        var pics = [Karateka.pictureRight, 'res/kw1.png', 'res/kw2.png', 'res/kw3.png'];
+        this.steps = new CircularList(pics);
+    }
+    Karateka.prototype.flip = function () {
+        this.speed = -this.speed;
+        if (this.picture == Karateka.pictureLeft)
+            this.picture = Karateka.pictureRight;
+        else
+            this.picture = Karateka.pictureLeft;
+        this.setPicture();
+    };
+    Karateka.prototype.move = function () {
+        this.x += this.speed;
+        var karatekaElement = this.getWebElement();
+        karatekaElement.style.left = "".concat(this.x, "px");
+        if (this.x % (4 * this.speed) > 3) {
+            this.picture = this.steps.next();
+        }
+        console.log(this.picture);
+        this.setPicture();
+    };
+    Karateka.prototype.setPicture = function () {
+        var k = this.getWebElement();
+        k.setAttribute("src", this.picture);
+    };
+    Karateka.prototype.getWebElement = function () {
+        return document.getElementById('karateka');
+    };
+    Karateka.pictureLeft = "res/kL.png";
+    Karateka.pictureRight = "res/k.png";
+    return Karateka;
+}());
 function ikpostToFront() {
     var ikpost = document.getElementById('ik-cover');
     ikpost.style.zIndex = '3';
 }
-console.log('hello');
-function animate(currentTime) {
+function moveKarateka(currentTime) {
     var timeSinceLastRender = currentTime - lastRenderTime;
     if (timeSinceLastRender > 100 / fps) {
-        moveKarateka();
-        ikpostToFront();
+        karateka.move();
+        if (karateka.x < 260 || karateka.x > 1920)
+            karateka.flip();
         lastRenderTime = currentTime;
     }
+}
+console.log('hello');
+function animate(currentTime) {
+    moveKarateka(currentTime);
     requestAnimationFrame(animate);
 }
+var karateka = new Karateka();
 requestAnimationFrame(animate);
--- other/2screen-ik/ik.ts
+++ other/2screen-ik/ik.ts
@@ -1,14 +1,69 @@
 const fps = 1;
-var x = 260;
 let lastRenderTime = 0;
-var speed = 15;
 
-function moveKarateka(){
+class CircularList<T> {
+    private list: T[];
+    private index: number;
+
+    constructor(list: T[]) {
+        this.list = list;
+        this.index = 0;
+    }
+
+    public next(): T {
+        let result = this.list[this.index];
+        this.index = (this.index + 1) % this.list.length;
+        return result;
+    }
+}
+
+class Karateka{
+
+	static readonly pictureLeft = "res/kL.png";
+	static readonly pictureRight = "res/k.png";
 	
-	var karateka = document.getElementById('karateka');
-	karateka.style.left = `${x}px`;
-    x+=speed;
-	console.log('' + karateka.style.left);
+	private speed = 15;
+	public x = 260;
+	public y = 260;
+	private steps: CircularList<string>;
+	private picture = Karateka.pictureRight;
+	
+	constructor(){
+		let pics = [Karateka.pictureRight, 'res/kw1.png', 'res/kw2.png', 'res/kw3.png'];
+		
+		this.steps = new CircularList(pics);
+	}
+	
+	flip(){
+		this.speed = -this.speed;
+		
+		if (this.picture == Karateka.pictureLeft) this.picture = Karateka.pictureRight
+			else this.picture = Karateka.pictureLeft;
+		
+		this.setPicture();
+	}
+	
+	move(){
+		this.x += this.speed;
+		var karatekaElement = this.getWebElement();
+		karatekaElement.style.left = `${this.x}px`;
+		if (this.x % (4* this.speed) > 3){
+			this.picture = this.steps.next();
+		}
+		
+		console.log(this.picture);
+		
+		this.setPicture();
+	}
+	
+	setPicture(){
+		var k = this.getWebElement();
+		k.setAttribute("src", this.picture);
+	}
+	
+	getWebElement(){
+		return document.getElementById('karateka');
+	}
 }
 
 function ikpostToFront(){
@@ -16,18 +71,26 @@ function ikpostToFront(){
 	ikpost.style.zIndex = '3';
 }
 
+function moveKarateka(currentTime){
+    const timeSinceLastRender = currentTime - lastRenderTime;
+
+    if (timeSinceLastRender > 100 / fps) {
+		karateka.move();
+		
+		if (karateka.x < 260 || karateka.x > 1920)
+			karateka.flip();
+		
+		lastRenderTime = currentTime;
+    }
+}
+
 console.log('hello');
 
 
 function animate(currentTime) {
-    const timeSinceLastRender = currentTime - lastRenderTime;
-
-    if (timeSinceLastRender > 100 / fps) {
-		moveKarateka();
-		ikpostToFront();
-		lastRenderTime = currentTime;
-    }
+	moveKarateka(currentTime);
     requestAnimationFrame(animate);
 }
 
+const karateka = new Karateka();
 requestAnimationFrame(animate);
