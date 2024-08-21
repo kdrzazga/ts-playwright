da0ec4886a3e53079bc4148f632a5f491b131872
kdrzazga <kdrzazga@luxoft.com>
2024-08-21 05:51:47 +0200

    commando-optimization

--- other/6/commando64/c64.js
+++ other/6/commando64/c64.js
@@ -12,90 +12,76 @@ var CircularList = /** @class */ (function () {
 }());
 var Commodore64 = /** @class */ (function () {
     function Commodore64() {
-        this.blink = true;
+        this.blinking = true;
+        this.commandoImgChangeCounter = 0;
         this.welcomeScreenTimeoutCounter = Commodore64.welcomeScreenTimeoutCounterMax;
         this.commandoTimeoutCounter = Commodore64.commandoTimeoutCounterMax;
         this.commandoReloadCounter = Commodore64.commandoReloadCounterMax;
+        this.animationFrameID = null;
         this.tableContentHeader = [
             "&nbsp",
-            "<center>    &nbsp**** COMMODORE 64 BASIC V2 ****&nbsp    </center>",
+            "<center>&nbsp**** COMMODORE 64 BASIC V2 ****&nbsp</center>",
             "&nbsp",
-            "<center> &nbsp64K RAM SYSTEM  38911 BASIC BYTES FREE&nbsp </center>"
+            "<center>&nbsp64K RAM SYSTEM  38911 BASIC BYTES FREE&nbsp</center>"
         ];
         this.tableContent = [
-            "&nbsp",
-            "READY.",
-            "LOAD",
-            "&nbsp",
-            "PRESS PLAY ON TAPE",
-            "OK",
-            "&nbsp",
-            "SEARCHING",
-            "FOUND COMMANDO",
-            "LOADING",
-            "READY",
-            "RUN",
-            "&nbsp",
-            "READY"
+            "&nbsp", "READY.", "LOAD", "&nbsp",
+            "PRESS PLAY ON TAPE", "OK", "&nbsp",
+            "SEARCHING", "FOUND COMMANDO", "LOADING",
+            "READY", "RUN", "&nbsp", "READY"
         ];
-        for (var i = 17; i < 20; i++) {
+        for (var i = 0; i < 3; i++) {
             this.tableContent.push("&nbsp");
         }
         this.lastRenderTime = 0;
         this.canvas = document.createElement('canvas');
         this.canvas.style.position = 'relative';
-        this.canvas.style.top = '-2px';
-        this.canvas.style.left = '-16px';
+        this.canvas.style.cssText = 'top: -2px; left: -16px;';
         this.canvas.width = 16;
         this.canvas.height = 16;
         this.ctx = this.canvas.getContext('2d');
-        this.commandoImages = new CircularList(['resources/LU.png', 'resources/U.png', 'resources/RU.png']);
+        this.commandoImages = new CircularList([
+            'resources/LU.png',
+            'resources/U.png',
+            'resources/RU.png'
+        ]);
         this.currentCommandoImg = this.commandoImages.next();
     }
     Commodore64.prototype.generateHtml = function () {
-        var html = [];
         var number = 0;
-        html.push("<table id=\"main\" bgcolor=\"" + Commodore64.BLUE + "\">");
+        var html = [];
+        html.push("<table id=\"main\" bgcolor=\"".concat(Commodore64.BLUE, "\">"));
         this.tableContentHeader.forEach(function (line) {
-            var strNumber = String(number);
             var style = line === "&nbsp" ? "" : "style='white-space: nowrap;'";
-            html.push("<tr><td id=\"row" + strNumber + "\" colspan=\"2\" " + style + ">" + line + "</td></tr>");
+            html.push("<tr><td id=\"row".concat(number, "\" colspan=\"2\" ").concat(style, ">").concat(line, "</td></tr>"));
             number++;
         });
         this.tableContent.forEach(function (line) {
-            var strNumber = String(number);
-            html.push("<tr><td id=\"row" + strNumber + "col1\">" + line + "</td><td id=\"row" + strNumber + "col2\"></td></tr>");
+            html.push("<tr><td id=\"row".concat(number, "col1\">").concat(line, "</td><td id=\"row").concat(number, "col2\"></td></tr>"));
             number++;
         });
-        html.push("</table>");
+        html.push('</table>');
         return html.join("");
     };
     Commodore64.prototype.generateBorder = function () {
-        var html = [];
-        html.push("<table bgcolor=\"" + Commodore64.LIGHTBLUE + "\" width=\"100%\">");
-        for (var i = 0; i < 3; i++) {
-            html.push("<tr><td width=\"10%\">&nbsp</td><td width=\"80%\">&nbsp</td><td width=\"10%\">&nbsp</td></tr>");
-        }
-        html.push("</table>");
-        return html.join("");
+        return "\n        <table bgcolor=\"".concat(Commodore64.LIGHTBLUE, "\" width=\"100%\">\n            ").concat('<tr><td width="10%">&nbsp</td><td width="80%">&nbsp</td><td width="10%">&nbsp</td></tr>'.repeat(3), "\n        </table>");
     };
     Commodore64.prototype.initBlinker = function () {
-        this.canvasContainer = document.getElementById('row18col1');
-        this.canvasContainer.appendChild(this.canvas);
+        var canvasContainer = document.getElementById('row18col1');
+        canvasContainer === null || canvasContainer === void 0 ? void 0 : canvasContainer.appendChild(this.canvas);
     };
-    Commodore64.prototype.blinker = function () {
+    Commodore64.prototype.blink = function () {
         var _this = this;
         var currentTime = performance.now();
         var timeSinceLastRender = currentTime - this.lastRenderTime;
         if (timeSinceLastRender >= 1000 / Commodore64.FPS) {
             this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-            this.ctx.fillStyle = this.blink ? Commodore64.LIGHTBLUE : Commodore64.BLUE;
+            this.ctx.fillStyle = this.blinking ? Commodore64.LIGHTBLUE : Commodore64.BLUE;
             this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-            this.blink = !this.blink;
+            this.blinking = !this.blinking;
             this.lastRenderTime = currentTime;
         }
-        //console.log("blink = " + this.blink);
-        this.animationFrameID = requestAnimationFrame(function () { return _this.blinker(); });
+        this.animationFrameID = requestAnimationFrame(function () { return _this.blink(); });
         this.welcomeScreenTimeoutCounter--;
         if (this.welcomeScreenTimeoutCounter < 0) {
             cancelAnimationFrame(this.animationFrameID);
@@ -105,7 +91,6 @@ var Commodore64 = /** @class */ (function () {
     Commodore64.prototype.rotateCommando = function () {
         var _this = this;
         var currentTime = performance.now();
-        var timeSinceLastRender = currentTime - this.lastRenderTime;
         var commandoWebElement = document.getElementById('commando');
         commandoWebElement.setAttribute("src", this.currentCommandoImg);
         this.commandoImgChangeCounter++;
@@ -122,37 +107,39 @@ var Commodore64 = /** @class */ (function () {
     };
     Commodore64.prototype.drawKomandos = function () {
         var komandosCell = document.getElementById('row5col2');
-        komandosCell.innerHTML = "<img src = \"resources/komandos.png\"/>";
-        komandosCell.setAttribute("rowspan", "12");
+        if (komandosCell) {
+            komandosCell.innerHTML = "<img src='resources/komandos.png'/>";
+            komandosCell.setAttribute("rowspan", "12");
+        }
     };
     Commodore64.prototype.initLoader = function () {
-        var html = this.generateHtml();
         var div = document.getElementById('commodore64');
         var topBorderDiv = document.getElementById('top-border');
         var bottomBorderDiv = document.getElementById('bottom-border');
-        div.innerHTML = html;
-        topBorderDiv.innerHTML = this.generateBorder();
-        bottomBorderDiv.innerHTML = this.generateBorder();
-        this.drawKomandos();
-        this.initBlinker();
-        this.blinker();
+        if (div && topBorderDiv && bottomBorderDiv) {
+            div.innerHTML = this.generateHtml();
+            topBorderDiv.innerHTML = this.generateBorder();
+            bottomBorderDiv.innerHTML = this.generateBorder();
+            this.drawKomandos();
+            this.initBlinker();
+            this.blink();
+        }
     };
     Commodore64.prototype.initGame = function () {
         this.commandoTimeoutCounter = Commodore64.commandoTimeoutCounterMax;
         var board = document.getElementById('commodore64');
-        board.innerHTML = "<img src = 'resources/board.png' style='width: 150%; height: 150%;'></img>";
-        board.innerHTML += "<img id='commando' src = 'resources/U.PNG' style='position: absolute; top: 54%; left: 47%; z-index: 1;'></img>";
-        board.innerHTML += "<img id='grenade' src = 'resources/bottle.PNG' style='position: absolute; top: 24%; left: 25%; z-index: 1; width: calc(10% * 0.3); height: calc(30% * 0.3);'></img>";
-        var grenade = document.getElementById('grenade');
-        this.grenadeX = grenade.offsetTop; //offsetTop is read-only
-        this.commandoImgChangeCounter = grenade.offsetLeft;
-        var commandoImg = document.getElementById('commando');
-        this.rotateCommando();
-        this.commandoReloadCounter--;
-        console.log(this.commandoReloadCounter);
-        if (this.commandoReloadCounter < 0) {
-            console.log("----------------------------------RELOADING COMMANDO----------------------------------");
-            location.reload();
+        if (board) {
+            board.innerHTML = "\n            <img src='resources/board.png' style='width: 150%; height: 150%;'></img>\n            <img id='commando' src='resources/U.PNG' style='position: absolute; top: 54%; left: 47%; z-index: 1;'></img>\n            <img id='grenade' src='resources/bottle.PNG' style='position: absolute; top: 24%; left: 25%; z-index: 1; width: calc(10% * 0.3); height: calc(30% * 0.3);'></img>\n            ";
+            var grenade = document.getElementById('grenade');
+            this.grenadeX = grenade.offsetTop; // offsetTop is read-only
+            this.commandoImgChangeCounter = grenade.offsetLeft;
+            this.rotateCommando();
+            this.commandoReloadCounter--;
+            console.log(this.commandoReloadCounter);
+            if (this.commandoReloadCounter < 0) {
+                console.log("----------------------------------RELOADING COMMANDO----------------------------------");
+                location.reload();
+            }
         }
     };
     Commodore64.FPS = 2;
--- other/6/commando64/c64.ts
+++ other/6/commando64/c64.ts
@@ -8,206 +8,191 @@ class CircularList<T> {
     }
 
     public next(): T {
-        let result = this.list[this.index];
+        const result = this.list[this.index];
         this.index = (this.index + 1) % this.list.length;
         return result;
     }
 }
 
 class Commodore64 {
-  static FPS = 2;
-  static BLUE = "#352879";
-  static LIGHTBLUE = "#6c5eb5";
-  static welcomeScreenTimeoutCounterMax = 1520;
-  static commandoTimeoutCounterMax = 300;
-  static commandoReloadCounterMax = 2;
+    static FPS = 2;
+    static BLUE = "#352879";
+    static LIGHTBLUE = "#6c5eb5";
+    static welcomeScreenTimeoutCounterMax = 1520;
+    static commandoTimeoutCounterMax = 300;
+    static commandoReloadCounterMax = 2;
 
-  private tableContentHeader: string[];
-  private tableContent: string[];
-  private blink = true;
-  private lastRenderTime;
-  private canvas;
-  private canvasContainer;
-  private ctx;
-  private welcomeScreenTimeoutCounter = Commodore64.welcomeScreenTimeoutCounterMax;
-  private commandoTimeoutCounter = Commodore64.commandoTimeoutCounterMax;
-  private commandoReloadCounter = Commodore64.commandoReloadCounterMax;
-  private animationFrameID;
-  private grenadeX: number;
-  private grenadeY: number;
-  private currentCommandoImg: string;
-  private commandoImages;
-  private commandoImgChangeCounter;
+    private tableContentHeader: string[];
+    private tableContent: string[];
+    private blinking = true;
+    private lastRenderTime: number;
+    private canvas: HTMLCanvasElement;
+    private ctx: CanvasRenderingContext2D | null;
+    private commandoImages: CircularList<string>;
+    private currentCommandoImg: string;
+    private commandoImgChangeCounter = 0;
+    private welcomeScreenTimeoutCounter = Commodore64.welcomeScreenTimeoutCounterMax;
+    private commandoTimeoutCounter = Commodore64.commandoTimeoutCounterMax;
+    private commandoReloadCounter = Commodore64.commandoReloadCounterMax;
+    private animationFrameID: number | null = null;
+	private grenadeX;
 
-  constructor() {
-    this.tableContentHeader = [
-      "&nbsp",		
-      "<center>    &nbsp**** COMMODORE 64 BASIC V2 ****&nbsp    </center>",
-      "&nbsp",
-      "<center> &nbsp64K RAM SYSTEM  38911 BASIC BYTES FREE&nbsp </center>"
-	];
-	
-	this.tableContent = [
-	  "&nbsp",
-	  "READY.",
-	  "LOAD",
-	  "&nbsp",
-	  "PRESS PLAY ON TAPE",
-	  "OK",
-	  "&nbsp",
-	  "SEARCHING",
-	  "FOUND COMMANDO",
-	  "LOADING",
-	  "READY",
-	  "RUN",
-	  "&nbsp",
-	  "READY"
-	  
-    ];
-	
-	for (let i = 17; i < 20; i++) {
-      this.tableContent.push("&nbsp");
+    constructor() {
+        this.tableContentHeader = [
+            "&nbsp",		
+            "<center>&nbsp**** COMMODORE 64 BASIC V2 ****&nbsp</center>",
+            "&nbsp",
+            "<center>&nbsp64K RAM SYSTEM  38911 BASIC BYTES FREE&nbsp</center>"
+        ];
+    
+        this.tableContent = [
+            "&nbsp", "READY.", "LOAD", "&nbsp",
+            "PRESS PLAY ON TAPE", "OK", "&nbsp",
+            "SEARCHING", "FOUND COMMANDO", "LOADING",
+            "READY", "RUN", "&nbsp", "READY"
+        ];
+
+        for (let i = 0; i < 3; i++) {
+            this.tableContent.push("&nbsp");
+        }
+        
+        this.lastRenderTime = 0;
+        this.canvas = document.createElement('canvas');
+        this.canvas.style.position = 'relative';
+        this.canvas.style.cssText = 'top: -2px; left: -16px;';
+        this.canvas.width = 16;
+        this.canvas.height = 16;
+        this.ctx = this.canvas.getContext('2d')!;
+        
+        this.commandoImages = new CircularList([
+            'resources/LU.png', 
+            'resources/U.png', 
+            'resources/RU.png'
+        ]);
+        this.currentCommandoImg = this.commandoImages.next();
     }
-	this.lastRenderTime = 0;
-	
-	this.canvas = document.createElement('canvas');
-	this.canvas.style.position = 'relative';
-	this.canvas.style.top = '-2px';
-	this.canvas.style.left = '-16px';
-	this.canvas.width = 16;
-	this.canvas.height = 16;
-	this.ctx = this.canvas.getContext('2d');
-	
-	this.commandoImages = new CircularList(['resources/LU.png', 'resources/U.png', 'resources/RU.png']);
-	this.currentCommandoImg = this.commandoImages.next();
-  }
 
-  generateHtml(): string {
-    const html = [];
-	let number = 0;
-    html.push("<table id=\"main\" bgcolor=\"" + Commodore64.BLUE + "\">");
-    this.tableContentHeader.forEach((line) => {
-	  let strNumber = String(number);
-	  let style = line === "&nbsp" ? "" : "style='white-space: nowrap;'";
-	  html.push("<tr><td id=\"row" + strNumber + "\" colspan=\"2\" " + style + ">" + line + "</td></tr>");
-	  number++;
-    });
-    this.tableContent.forEach((line) => {
-	  let strNumber = String(number);
-      html.push("<tr><td id=\"row" + strNumber + "col1\">" + line + "</td><td id=\"row" + strNumber + "col2\"></td></tr>");
-	  number++;
-    });
-    html.push("</table>");
-    return html.join("");
-  }
+    private generateHtml(): string {
+        let number = 0;
+        const html = [];
+        html.push(`<table id="main" bgcolor="${Commodore64.BLUE}">`);
+        this.tableContentHeader.forEach((line) => {
+            const style = line === "&nbsp" ? "" : "style='white-space: nowrap;'";
+            html.push(`<tr><td id="row${number}" colspan="2" ${style}>${line}</td></tr>`);
+            number++;
+        });
+        this.tableContent.forEach((line) => {
+            html.push(`<tr><td id="row${number}col1">${line}</td><td id="row${number}col2"></td></tr>`);
+            number++;
+        });
+        html.push('</table>');
+        return html.join("");
+    }
   
-  generateBorder(): string {
-    const html = [];
-	html.push("<table bgcolor=\"" + Commodore64.LIGHTBLUE + "\" width=\"100%\">")
-	for (let i = 0; i < 3; i++){
-		html.push("<tr><td width=\"10%\">&nbsp</td><td width=\"80%\">&nbsp</td><td width=\"10%\">&nbsp</td></tr>");
-	}
-	html.push("</table>");
-    return html.join("");  
-  }
-  
-  initBlinker(){
-	this.canvasContainer = document.getElementById('row18col1');
-	this.canvasContainer.appendChild(this.canvas);
-  }
-  
-  blinker() {
-	const currentTime = performance.now();
-	const timeSinceLastRender = currentTime - this.lastRenderTime;
-	
-	if (timeSinceLastRender >= 1000 / Commodore64.FPS) {
-	    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.fillStyle = this.blink ? Commodore64.LIGHTBLUE : Commodore64.BLUE;
-		
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-		this.blink = !this.blink;
-		this.lastRenderTime = currentTime;
-	}
-	
-	//console.log("blink = " + this.blink);
-    this.animationFrameID = requestAnimationFrame(() => this.blinker());
-	
-	this.welcomeScreenTimeoutCounter--;
-	
-	if (this.welcomeScreenTimeoutCounter < 0){
-		cancelAnimationFrame(this.animationFrameID);
-		this.initGame();
-	}
-  }
-  
-  rotateCommando() {
-	const currentTime = performance.now();
-	const timeSinceLastRender = currentTime - this.lastRenderTime;
-	
-	const commandoWebElement = document.getElementById('commando');
-	commandoWebElement.setAttribute("src", this.currentCommandoImg);
+    private generateBorder(): string {
+        return `
+        <table bgcolor="${Commodore64.LIGHTBLUE}" width="100%">
+            ${'<tr><td width="10%">&nbsp</td><td width="80%">&nbsp</td><td width="10%">&nbsp</td></tr>'.repeat(3)}
+        </table>`;
+    }
 
-	this.commandoImgChangeCounter++;
-	
-	if (this.commandoImgChangeCounter > 32) {
-		this.currentCommandoImg = this.commandoImages.next();
-		this.commandoImgChangeCounter = 0;
-	}
-	
-    this.animationFrameID = requestAnimationFrame(() => this.rotateCommando());
-	
-	this.commandoTimeoutCounter--;
-	
-	if (this.commandoTimeoutCounter < 0){
-		cancelAnimationFrame(this.animationFrameID);
-		this.initGame();
-	}
-  }
-  
-  drawKomandos(){
-	let komandosCell = document.getElementById('row5col2');
-	komandosCell.innerHTML = "<img src = \"resources/komandos.png\"/>";
-	komandosCell.setAttribute("rowspan", "12");
-  }
-  
-  initLoader(){
-	const html = this.generateHtml();
-	const div = document.getElementById('commodore64');
-	const topBorderDiv = document.getElementById('top-border');
-	const bottomBorderDiv = document.getElementById('bottom-border');
-	div.innerHTML = html;
-	topBorderDiv.innerHTML = this.generateBorder();
-	bottomBorderDiv.innerHTML = this.generateBorder();
-	this.drawKomandos();
-	this.initBlinker();
-	this.blinker();
-  }
-  
-  initGame(){
-	this.commandoTimeoutCounter = Commodore64.commandoTimeoutCounterMax;
-	const board = document.getElementById('commodore64');
-	board.innerHTML = "<img src = 'resources/board.png' style='width: 150%; height: 150%;'></img>";
-	board.innerHTML += "<img id='commando' src = 'resources/U.PNG' style='position: absolute; top: 54%; left: 47%; z-index: 1;'></img>";
-	board.innerHTML += "<img id='grenade' src = 'resources/bottle.PNG' style='position: absolute; top: 24%; left: 25%; z-index: 1; width: calc(10% * 0.3); height: calc(30% * 0.3);'></img>";
-	const grenade = document.getElementById('grenade');
-	this.grenadeX = grenade.offsetTop;//offsetTop is read-only
-	this.commandoImgChangeCounter = grenade.offsetLeft;
-	
-	const commandoImg = document.getElementById('commando');
-	
-	this.rotateCommando();
-	this.commandoReloadCounter--;
-	console.log(this.commandoReloadCounter);
-	
-	if (this.commandoReloadCounter < 0){
-		console.log("----------------------------------RELOADING COMMANDO----------------------------------");
-		location.reload();
-	}
-  }
+    private initBlinker() {
+        const canvasContainer = document.getElementById('row18col1');
+        canvasContainer?.appendChild(this.canvas);
+    }
+
+    private blink() {
+        const currentTime = performance.now();
+        const timeSinceLastRender = currentTime - this.lastRenderTime;
+
+        if (timeSinceLastRender >= 1000 / Commodore64.FPS) {
+            this.ctx!.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            this.ctx!.fillStyle = this.blinking ? Commodore64.LIGHTBLUE : Commodore64.BLUE;
+            this.ctx!.fillRect(0, 0, this.canvas.width, this.canvas.height);
+            this.blinking = !this.blinking;
+            this.lastRenderTime = currentTime;
+        }
+
+        this.animationFrameID = requestAnimationFrame(() => this.blink());
+        this.welcomeScreenTimeoutCounter--;
+
+        if (this.welcomeScreenTimeoutCounter < 0) {
+            cancelAnimationFrame(this.animationFrameID!);
+            this.initGame();
+        }
+    }
+
+    private rotateCommando() {
+        const currentTime = performance.now();
+        const commandoWebElement = document.getElementById('commando');
+        commandoWebElement!.setAttribute("src", this.currentCommandoImg);
+
+        this.commandoImgChangeCounter++;
+        if (this.commandoImgChangeCounter > 32) {
+            this.currentCommandoImg = this.commandoImages.next();
+            this.commandoImgChangeCounter = 0;
+        }
+
+        this.animationFrameID = requestAnimationFrame(() => this.rotateCommando());
+        this.commandoTimeoutCounter--;
+
+        if (this.commandoTimeoutCounter < 0) {
+            cancelAnimationFrame(this.animationFrameID!);
+            this.initGame();
+        }
+    }
+
+    private drawKomandos() {
+        const komandosCell = document.getElementById('row5col2');
+        if (komandosCell) {
+            komandosCell.innerHTML = "<img src='resources/komandos.png'/>";
+            komandosCell.setAttribute("rowspan", "12");
+        }
+    }
+
+    public initLoader() {
+        const div = document.getElementById('commodore64');
+        const topBorderDiv = document.getElementById('top-border');
+        const bottomBorderDiv = document.getElementById('bottom-border');
+
+        if (div && topBorderDiv && bottomBorderDiv) {
+            div.innerHTML = this.generateHtml();
+            topBorderDiv.innerHTML = this.generateBorder();
+            bottomBorderDiv.innerHTML = this.generateBorder();
+            this.drawKomandos();
+            this.initBlinker();
+            this.blink();
+        }
+    }
+
+    private initGame() {
+        this.commandoTimeoutCounter = Commodore64.commandoTimeoutCounterMax;
+        
+        const board = document.getElementById('commodore64');
+        if (board) {
+            board.innerHTML = `
+            <img src='resources/board.png' style='width: 150%; height: 150%;'></img>
+            <img id='commando' src='resources/U.PNG' style='position: absolute; top: 54%; left: 47%; z-index: 1;'></img>
+            <img id='grenade' src='resources/bottle.PNG' style='position: absolute; top: 24%; left: 25%; z-index: 1; width: calc(10% * 0.3); height: calc(30% * 0.3);'></img>
+            `;
+            const grenade = document.getElementById('grenade')!;
+            this.grenadeX = grenade.offsetTop;  // offsetTop is read-only
+            this.commandoImgChangeCounter = grenade.offsetLeft;
+
+            this.rotateCommando();
+            this.commandoReloadCounter--;
+            console.log(this.commandoReloadCounter);
+
+            if (this.commandoReloadCounter < 0) {
+                console.log("----------------------------------RELOADING COMMANDO----------------------------------");
+                location.reload();
+            }
+        }
+    }
 }
 
-let width = 800;
-let height = 600;
+const width = 800;
+const height = 600;
 
 window.innerWidth = width;
 window.innerHeight = height;
