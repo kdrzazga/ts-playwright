cb9c70b91b7c813572717b0cb3d714d0fa4a3385
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-10-02 21:03:58 +0200

    Update script.js

--- other/cube/script.js
+++ other/cube/script.js
@@ -1,42 +1,59 @@
-// script.js
-// Create a scene
+const speedVerticalSlider = document.getElementById('speed-vertical');
+const speedHorizontalSlider = document.getElementById('speed-horizontal');
+const changeColorButton = document.getElementById('change-color');
+
 const scene = new THREE.Scene();
 
-// Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
-// Create a renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Create a filled cube
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-// Create a wireframe cube
-const wireframeMaterial = new THREE.LineBasicMaterial({ color: 0x000000 }); // Red color for the wireframe
+const wireframeMaterial = new THREE.LineBasicMaterial({ color: 0x000000 }); // black color for the wireframe
 const wireframe = new THREE.LineSegments(new THREE.EdgesGeometry(geometry), wireframeMaterial);
+
+let speedX = 0.01
+let speedY = 0.01
 scene.add(wireframe);
 
-// Animation loop
+const predefinedColors = [
+    0xff0000, // Red
+    0x00ff00, // Green
+    0x0000ff, // Blue
+    0xffff00, // Yellow
+    0xff00ff, // Magenta
+    0x00ffff, // Cyan
+    0xaaaaaa, // gray
+    0xffffff  // White
+];
+
 function animate() {
     requestAnimationFrame(animate);
+	
+	speedX = speedVerticalSlider.value / 100;
+	speedY = speedHorizontalSlider.value / 100;
 
-    // Rotate both cubes
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    wireframe.rotation.x += 0.01;
-    wireframe.rotation.y += 0.01;
+    cube.rotation.x += speedX;
+    cube.rotation.y += speedY;
+    wireframe.rotation.x += speedX;
+    wireframe.rotation.y += speedY;
 
-    // Render the scene from the perspective of the camera
     renderer.render(scene, camera);
 }
 
-// Handle window resizing
+changeColorButton.addEventListener('click', () => {
+    const randomIndex = Math.floor(Math.random() * predefinedColors.length); 
+    const randomColor = predefinedColors[randomIndex]; 
+    cube.material.color.set(randomColor); 
+});
+
 window.addEventListener('resize', () => {
     const width = window.innerWidth;
     const height = window.innerHeight;
@@ -45,5 +62,4 @@ window.addEventListener('resize', () => {
     camera.updateProjectionMatrix();
 });
 
-// Start the animation
 animate();
