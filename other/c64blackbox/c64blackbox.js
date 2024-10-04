2b67651c984dd95b978187c6ed9dec553593bfd1
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-10-04 14:02:29 +0200

    fucntion keys

--- other/c64blackbox/c64blackbox.js
+++ other/c64blackbox/c64blackbox.js
@@ -1,5 +1,7 @@
 class C64Blackbox {
     static backgroundColor = '#979797';
+    static lightgrayColor = '#979797';
+    static yellowColor = '#ffff00';
     static rowHeight = 20;
 
     constructor() {
@@ -13,6 +15,7 @@ class C64Blackbox {
         this.cursorSize = C64Blackbox.rowHeight - 7;
         this.cursorPosition = { x: Math.floor(this.cursorSize / 2) + 1, y: 6.5 * C64Blackbox.rowHeight };
         this.cursorVisible = true;
+        this.texture = null; // Initialize texture to null
 
         this.init();
     }
@@ -27,29 +30,80 @@ class C64Blackbox {
         canvas.width = 520;
         canvas.height = 512;
 
+        this.texture = new THREE.CanvasTexture(canvas); // Create texture
+        const planeMaterial = new THREE.MeshBasicMaterial({ map: this.texture });
+        this.plane = new THREE.Mesh(this.planeGeometry, planeMaterial);
+        this.plane.rotation.x = -Math.PI / 12;
+		this.drawInitialText(context); // Draw initial text on the canvas
+        this.scene.add(this.plane);
+
+        this.camera.position.z = 5;
+
+        window.addEventListener('keydown', this.handleKeyDown.bind(this));
+        this.animate();
+    }
+
+    drawInitialText(context) {
+		C64Blackbox.backgroundColor = C64Blackbox.lightgrayColor;
         context.fillStyle = '#494949';
-        context.fillRect(0, 0, canvas.width, canvas.height);
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
         context.fillStyle = C64Blackbox.backgroundColor;
-        context.fillRect(0, 90, canvas.width, canvas.height);
+        context.fillRect(0, 90, context.canvas.width, context.canvas.height);
         context.fillStyle = '#979797';
         context.font = '13px c64mono';
         context.fillText('* C-64 BASIC IMPROVED BY BLACK BOX V.3 *', 0, 2 * C64Blackbox.rowHeight);
         context.fillText('64K RAM SYSTEM   38911  BASIC BYTES FREE', 0, 4 * C64Blackbox.rowHeight);
         context.fillStyle = 'black';
         context.fillText('READY.', 0, 120);
-
-        this.texture = new THREE.CanvasTexture(canvas);
-        const planeMaterial = new THREE.MeshBasicMaterial({ map: this.texture });
-        this.plane = new THREE.Mesh(this.planeGeometry, planeMaterial);
-        this.plane.rotation.x = -Math.PI / 12;
-        this.scene.add(this.plane);
-
-        this.camera.position.z = 5;
-
-		window.addEventListener('keydown', this.handleKeyDown.bind(this));
-        this.animate();
+		context.fillStyle = '#494949';
+        
+        this.texture.needsUpdate = true; // Notify the texture that it should update
     }
 
+    clearOutput() {
+
+        const context = this.texture.image.getContext('2d');
+        context.clearRect(0, 0, this.texture.image.width, this.texture.image.height);
+        this.drawInitialText(context); 
+        this.texture.needsUpdate = true;
+		
+		console.log('Output resetted. C64 screen redrawn.');
+    }
+
+    handleF1() {
+        console.log('1 or F1 was pressed');
+        this.clearOutput(); // Clear output when F1 is pressed
+    }
+
+    handleF2() {
+        console.log('2 or F2 was pressed');
+		C64Blackbox.backgroundColor = C64Blackbox.yellowColor;
+    }
+
+    handleF4() {
+        console.log('3, 4 or F4 was pressed');
+    }
+
+    handleKeyDown(event) {
+        const keyMapping = {
+            'F1': this.handleF1.bind(this),
+            'F2': this.handleF2.bind(this),
+            'F4': this.handleF4.bind(this)
+        };
+
+        const keyTriggers = {
+            'F1': ['F1', 112, '1'],
+            'F2': ['F2', 113, '2'],
+            'F4': ['F4', 115, '3', '4']
+        };
+
+        for (const [key, values] of Object.entries(keyTriggers)) {
+            if (values.includes(event.key) || values.includes(event.keyCode)) {
+                keyMapping[key]();
+                break;
+            }
+        }
+    }
     blinkCursor() {
         if (this.runningTime % this.blinkInterval == Math.floor(this.blinkInterval / 2)) {
             this.cursorVisible = true;
@@ -75,22 +129,7 @@ class C64Blackbox {
         this.texture.needsUpdate = true;
     }
 	
-	handleKeyDown(event) {
-		const keyMapping = {
-			'F1': ['F1', 112, '1', 'q', 'a'],
-			'F2': ['F2', 113, '2', 'w', 's'],
-			'F4': ['F4', 115, '3', '4', 'e', 'd']
-		};
-	
-		for (const [key, values] of Object.entries(keyMapping)) {
-			if (values.includes(event.key) || values.includes(event.keyCode)) {
-				console.log(`${key} or ${values.join(', ')} was pressed`);
-				break;
-			}
-		}
-	}
-
-    conditionalReset() {
+    conditionalRotationReset() {
         if (this.plane.rotation.y < -0.80 * Math.PI / 2 || this.plane.rotation.y > 0.80 * Math.PI / 2) {
             this.delta = -this.delta;
         }
@@ -102,7 +141,7 @@ class C64Blackbox {
         this.plane.rotation.y += this.delta;
 
         this.blinkCursor();
-        this.conditionalReset();
+        this.conditionalRotationReset();
 
         this.renderer.render(this.scene, this.camera);
     }
