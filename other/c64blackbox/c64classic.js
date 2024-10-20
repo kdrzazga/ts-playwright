6228016e552f7eacbfc3d679fc7afff9ee3057eb
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-10-20 11:07:22 +0200

    handle F2

--- other/c64blackbox/c64classic.js
+++ other/c64blackbox/c64classic.js
@@ -1,11 +1,12 @@
 class ClassicC64 extends C64Blackbox{
-	static backgroundColor = Globals.colors[6];	
+	static backgroundColor = Globals.colors[6];
     static secondaryBackgroundColor = Globals.colors[6];
 	
 	constructor(){
 		super();
 		this.clearColor = Globals.colors[14];		
 		this.defaultColor = Globals.colors[14];
+		this.backgroundColor = Globals.colors[6];
 		
 		this.classRef = ClassicC64;
 	}
@@ -16,7 +17,12 @@ class ClassicC64 extends C64Blackbox{
 			{ text: '64K RAM SYSTEM   38911  BASIC BYTES FREE', color: Globals.colors[14] },
 			{ text: 'READY.', color: Globals.colors[14] }
 		];
-    }	
+    }
+
+     handleF2() {
+         console.log('2 or F2 was pressed');
+         this.softReset(Globals.colors[6]);
+     }
 }
 
 const c64classic = new ClassicC64();
