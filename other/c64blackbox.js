e72fe87b4b9d7106bee9b56bad2a614a6ee3ec00
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-10-20 09:57:57 +0200

    green honda

---
+++ other/c64blackbox.js
@@ -0,0 +1,334 @@
+class Globals{
+	static runningTime = 0;	
+	
+    static lightgrayColor = '#b3b3b3';
+	static screenWidth = 520;
+	static screenHeight = 512;
+    static colors = ['black', 'white', 'red', 'cyan', 'magenta', 'green', '#4536a6', 'yellow', '#675200', '#c33d00', '#c18178', '#606060', '#8a8a8a', '#b3ec91', '#867ade', Globals.lightgrayColor];
+}
+
+const Direction = Object.freeze({
+    LEFT: 'left',
+    RIGHT: 'right',
+	UP: 'up',
+	DOWN: 'down'
+});
+
+class C64Blackbox {
+    static rowHeight = 20;
+	static currentColorIndex = 7;
+	static texture = null;	
+    static backgroundColor = Globals.lightgrayColor;
+    static secondaryBackgroundColor = Globals.colors[11];
+	static logoPictures = ['logo.png'];
+
+    constructor() {
+        this.scene = new THREE.Scene();
+        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.planeGeometry = new THREE.PlaneGeometry(5, 5);
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+        
+		this.delta = 0.006;  
+		this.headerLines = [];
+		this.context = null
+		this.cursor = null;
+		this.game = null;		
+		this.clearColor = Globals.colors[11];
+		this.backgroundColor = Globals.lightgrayColor;
+		this.defaultColor = 'black';
+		this.classRef = C64Blackbox;
+	}
+
+    init() {
+        this.setupRenderer();
+		this.setupHeaderContent();
+		
+        const canvas = document.createElement('canvas');
+        this.context = canvas.getContext('2d');
+        canvas.width = Globals.screenWidth;
+        canvas.height = Globals.screenHeight;
+		this.game = new Game(canvas);
+
+        C64Blackbox.texture = new THREE.CanvasTexture(canvas);
+		this.drawInitialText(this.context);
+		
+		this.setupPlane();
+
+        window.addEventListener('keydown', this.handleKeyDown.bind(this));
+        requestAnimationFrame(() => this.animate());
+    }
+
+    setupHeaderContent(){
+		this.headerLines = [
+			{ text: '* C-64 BASIC IMPROVED BY BLACK BOX V.3 *', color: Globals.lightgrayColor },
+			{ text: '64K RAM SYSTEM   38911  BASIC BYTES FREE', color: Globals.lightgrayColor },
+			{ text: 'READY.', color: 'black' }
+		];
+    }
+	
+    setupRenderer() {
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setClearColor(this.clearColor, 1);
+        document.body.appendChild(this.renderer.domElement);
+    }
+
+    setupPlane() {
+        const planeMaterial = new THREE.MeshBasicMaterial({ map: C64Blackbox.texture });
+        this.plane = new THREE.Mesh(this.planeGeometry, planeMaterial);
+        this.plane.rotation.x = -Math.PI / 12;
+        this.scene.add(this.plane);
+        this.camera.position.z = 5;
+    }
+
+    drawInitialText(context) {
+		this.cursor = new Cursor(context, this.defaultColor, this.backgroundColor);
+		
+        context.fillStyle = this.classRef.secondaryBackgroundColor;
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+        context.fillStyle = this.classRef.backgroundColor;
+        context.fillRect(0, 90, context.canvas.width, context.canvas.height);
+		
+	this.renderHeader();
+        C64Blackbox.texture.needsUpdate = true;
+    }
+	
+	renderHeader() {
+		this.context.font = '13px c64mono';
+
+		this.headerLines.forEach((line, index) => {
+			this.context.fillStyle = line.color;
+			this.context.fillText(line.text, 0, (2 + index * 2) * C64Blackbox.rowHeight);
+		});
+	}
+
+    clearOutput() {
+        const context = C64Blackbox.texture.image.getContext('2d');
+        context.clearRect(0, 0, C64Blackbox.texture.image.width, C64Blackbox.texture.image.height);
+        this.drawInitialText(context); 		
+		console.log('Output resetted. C64 screen redrawn.');
+    }
+	
+    clearOutputBottom(thresholdY) {
+        const context = C64Blackbox.texture.image.getContext('2d');
+		context.fillStyle = this.backgroundColor;
+        context.fillRect(0, thresholdY, C64Blackbox.texture.image.width, C64Blackbox.texture.image.height);
+		console.log('Bottom Output resetted.');
+    }
+	
+	handleHelp() {
+		console.log("HELP");
+		this.clearOutput();
+		
+		const context = C64Blackbox.texture.image.getContext('2d');
+		context.fillStyle = this.defaultColor;
+	
+		const helpTexts = [
+			[String.fromCharCode(0xe05f) + 'HELP', 2],
+			['F1, 1, Q - ' + String.fromCharCode(0xe05f) + 'HELP, DISPLAY THIS HELP', 1],
+			['F2, 2, 8, U, J - SOFT RESET', 1],
+			['F3, 3, 9, I, K - CHANGE BACKGROUND COLOR', 1],
+			['F6, 6, 0, =, O, L - ' + String.fromCharCode(0xe05f) + 'K&A+ LOGO', 1],
+			['F7, 7, -, P, ; - ' + String.fromCharCode(0xe05f) + 'BRUCE LEE SIMPLE GAME', 2],
+			['READY.', 1]
+		];
+	
+		for (const [text, movement] of helpTexts) {
+			context.fillText(text, 0, this.cursor.position.y + Math.floor(this.cursor.size / 2));
+			this.cursor.position.y += (this.cursor.size + 2) * movement;
+		}
+	}
+
+    handleF2() {
+        console.log('2 or F2 was pressed');
+		this.game.reset();
+		C64Blackbox.backgroundColor = Globals.lightgrayColor;
+        this.clearOutput();
+    }
+
+    handleF3() {
+        console.log('3, 9, I, K or F3 was pressed');
+		this.game.reset();
+		this.cursor.clear();
+		const context = C64Blackbox.texture.image.getContext('2d');
+		context.fillStyle = this.defaultColor;
+
+		context.fillText('POKE 53281, ' + C64Blackbox.currentColorIndex, 0, this.cursor.position.y + Math.floor(this.cursor.size / 2));
+		this.cursor.position.x = 15 * this.cursor.size - 3;
+		if (C64Blackbox.currentColorIndex < 10){
+			this.cursor.position.x -= this.cursor.size;
+		}
+		C64Blackbox.texture.needsUpdate = true;
+		
+		setTimeout(() => {
+		    this.backgroundColor = Globals.colors[C64Blackbox.currentColorIndex];
+			C64Blackbox.backgroundColor = this.backgroundColor;
+			C64Blackbox.currentColorIndex = (C64Blackbox.currentColorIndex + 1) % Globals.colors.length;
+			console.log(C64Blackbox.currentColorIndex);
+			this.clearOutput();
+		}, 1000);
+    }
+
+    handleF6() {
+        console.log('6, 0, =, O, L or F6 was pressed');
+		
+		this.game.reset();
+		this.clearOutput();
+		this.context.fillStyle = this.defaultColor;
+        this.context.fillText(String.fromCharCode(0xe05f) + 'K&A+', 0, 7 * C64Blackbox.rowHeight);
+        this.context.fillText('http://www.ka-plus.pl', 0, 13 * C64Blackbox.rowHeight + 2);
+        this.context.fillText('READY.', 0, 14 * C64Blackbox.rowHeight + 2);
+		this.cursor.position = { x: Math.floor(this.cursor.size / 2) + 1, y: 14.5 * C64Blackbox.rowHeight}
+		
+		this.loadPicture(C64Blackbox.logoPictures[0], 0, 8 * C64Blackbox.rowHeight);
+    }
+	
+	handleF7(){
+		console.log('7, -, P, ; or F7 was pressed. Simple game');
+		this.game.reset();
+		this.game.activate();		
+		this.clearOutputBottom(Math.floor(5 * Globals.screenHeight / 6));
+				
+		this.cursor.clear();
+		this.context.fillStyle = this.defaultColor;
+		this.context.fillText(String.fromCharCode(0xe05f) + 'BRUCE LEE', 0, this.cursor.position.y + 4);
+		this.cursor.moveDown(2);
+		this.context.fillStyle = this.defaultColor;
+		this.context.fillText('READY.', 0, this.cursor.position.y + 5);
+		this.cursor.moveDown(1);
+	}
+	
+	handleMovement(direction) {
+		this.clearOutputBottom(Math.floor(5 * Globals.screenHeight / 6));
+	
+		if (this.game.active) {
+			switch (direction) {
+				case Direction.UP:
+					console.log('UP key was pressed.');
+					break;
+				case Direction.DOWN:
+					console.log('DOWN key was pressed.');
+					break;
+				case Direction.LEFT:
+					console.log('LEFT key was pressed.');
+					this.game.moveFighterLeft(this.game.player);
+					break;
+				case Direction.RIGHT:
+					console.log('RIGHT key was pressed.');
+					this.game.moveFighterRight(this.game.player);
+					break;
+			}
+		}
+	}
+	
+	handleFire(){
+		if (this.game.active){
+			console.log('FIRE key was pressed.');
+			this.clearOutputBottom(Math.floor(5 * Globals.screenHeight / 6));
+			this.game.punch(this.game.player);
+		}		
+	}
+	
+	loadPicture(fileName, x, y) {
+		let pictureLoader = new PictureLoader(this.context);
+		pictureLoader.load(fileName, x, y);		
+		C64Blackbox.texture.needsUpdate = true;			
+	}
+
+    handleKeyDown(event) {
+        const keyMapping = {
+            help: () => this.handleHelp(),
+            'F2': () => this.handleF2(),
+            'F3': () => this.handleF3(),
+            'F6': () => this.handleF6(),
+            'F7': () => this.handleF7(),
+            w: () => this.handleMovement(Direction.UP),
+            s: () => this.handleMovement(Direction.DOWN),
+            a: () => this.handleMovement(Direction.LEFT),
+            d: () => this.handleMovement(Direction.RIGHT),
+            fire: () => this.handleFire(),
+        };
+
+        const keyTriggers = {
+            'help': ['F1', 111, '1', 'q'],
+            'F2': ['F2', 112, '2', '8', 'u', 'j'],
+            'F3': ['F3', 113, '3', '9', 'i', 'k'],
+            'F6': ['F6', 115, '6', '0', '=', 'o', 'l'],
+            'F7': ['F7', 117, '7', '-', 'p', ';'],
+			'w': ['w', 'ArrowUp'],
+			's': ['s', 'ArrowDown'],
+			'a': ['a', 'ArrowLeft'],
+			'd': ['d', 'ArrowRight'],
+			'fire' : ['Control', 17, 'Space', 32, 'Enter', 13]
+        };
+
+        for (const [key, values] of Object.entries(keyTriggers)) {
+            if (values.includes(event.key) || values.includes(event.keyCode)) {
+                keyMapping[key]();
+                break;
+            }
+        }
+    }
+
+    blinkCursor() {
+		this.cursor.blinkCursor();        
+        C64Blackbox.texture.needsUpdate = true;
+    }
+	
+    conditionalRotationReset() {
+        if (Math.abs(this.plane.rotation.y) > 0.80 * Math.PI / 2) {
+            this.delta *= -1;
+        }
+    }
+
+    animate() {
+        Globals.runningTime++;
+        this.plane.rotation.y += this.delta;
+        this.blinkCursor();
+        this.conditionalRotationReset();
+        this.renderer.render(this.scene, this.camera);
+		requestAnimationFrame(() => this.animate());
+    }
+}
+
+
+class Cursor{
+	
+	 static blinkInterval = 100;
+	
+	constructor(context, color, backgroundColor){
+		this.context = context;
+		this.color = color;
+		this.backgroundColor = backgroundColor;
+		this.size = C64Blackbox.rowHeight - 7;
+        this.position = { x: Math.floor(this.size / 2) + 1, y: 6.5 * C64Blackbox.rowHeight }
+		this.visible = true;
+	}
+	
+	moveDown(times){
+		this.position.y += times * this.size;
+	}
+	
+	clear(){		
+        var x = this.position.x - this.size / 2;
+        var y = this.position.y - this.size / 2;
+		
+		this.context.fillStyle = this.backgroundColor;
+        this.context.fillRect(x, y, this.size, this.size);	
+	}
+	
+    blinkCursor() {
+        const blinkState = Math.floor(Globals.runningTime / Cursor.blinkInterval) % 2;
+
+        const x = this.position.x - this.size / 2;
+        const y = this.position.y - this.size / 2;
+
+        // Clear the rectangle before drawing, to avoid ghosting
+        this.clear();
+
+        this.context.clearRect(x, y, this.size, this.size);
+        this.context.fillStyle = (blinkState === 0) ? this.color : this.backgroundColor;
+        this.context.fillRect(x, y, this.size, this.size);
+    }
+}
+
+const c64 = new C64Blackbox();
---
+++ other/minigame.js
@@ -0,0 +1,285 @@
+
+class PictureLoader{
+	
+	constructor(context){
+		this.context = context;
+		this.textureLoader = new THREE.TextureLoader();
+	}
+	
+	load(fileName, x, y) {
+	
+		this.textureLoader.load(fileName, (texture) => {
+			
+			const tmpCanvas = document.createElement('canvas');
+			const tmpCtx = tmpCanvas.getContext('2d');
+			
+			tmpCanvas.width = this.context.canvas.width;
+			tmpCanvas.height = this.context.canvas.height;
+			
+			tmpCtx.drawImage(this.context.canvas, 0, 0);
+			
+			tmpCtx.drawImage(texture.image, x, y);
+			
+			this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
+			this.context.drawImage(tmpCanvas, 0, 0);
+			
+			//console.log('Picture loaded and displayed at', x, y);
+		}, undefined, (error) => {
+			console.error('An error occurred while loading the texture:', error);
+		});
+	}
+	
+}
+
+class Fighter{
+	constructor(canvas){
+		this.speed = 3;
+		this.hp = 4;
+		this.x = 0;
+		this.y = Globals.screenHeight - 75;
+		this.name = "fighter";
+		this.canvas = canvas;
+		this.picPath = "";
+		this.picLeftPath = "";
+		this.picRightPath = "";
+		this.punchPicPath = "";
+		this.punchLeftPicPath = "";
+		this.punching = false;
+		this.punchAudio = new PunchAudio("punch.mp3");
+	}
+	
+	moveRight(){
+		if (this.hp > 0){
+			this.x += this.speed;			
+			this.picPath = this.picRightPath;
+			this.direction = Direction.RIGHT;
+		}
+	}
+	
+	moveLeft(){
+		if (this.hp > 0){
+			this.x -= this.speed;
+			this.picPath = this.picLeftPath;
+			this.direction = Direction.LEFT;
+		}
+	}
+	
+	moveBack(times){
+		if (this.direction == Direction.LEFT){
+			for (var i = 0; i < times; i++){
+				this.moveRight();
+			}
+		}
+		else {
+			for (var i = 0; i < times; i++){
+				this.moveLeft();
+			}
+		}
+	}
+	
+	draw(){
+		let context = this.canvas.getContext('2d');
+		let pictureLoader = new PictureLoader(context);
+		pictureLoader.load(this.picPath, this.x, this.y);        
+        C64Blackbox.texture.needsUpdate = true;
+	}
+	
+	punch(){
+		console.log('PUNCH');
+		let tempPicPath = this.picPath;
+		this.picPath = (this.direction == Direction.LEFT) ? this.punchLeftPicPath: this.punchPicPath;
+		this.draw();
+		this.punchAudio.enable();
+		this.punchAudio.playAudio();
+		
+		this.punching = true;
+		
+		setTimeout(() => {				
+			this.picPath = tempPicPath;
+			this.draw();
+			this.punching = false;
+		}, 500);
+		
+	}
+	
+	checkIfDead(){
+		if (this.hp <= 0){
+			console.log(this.name + " is dead.\n\n\n");
+			
+			let context = this.canvas.getContext('2d');
+			let pictureLoader = new PictureLoader(context);
+			pictureLoader.load('gameover.png', 8 * C64Blackbox.rowHeight, 4.75 * C64Blackbox.rowHeight);
+			C64Blackbox.texture.needsUpdate = true;	
+			
+			setTimeout(() => {
+				location.reload();
+			}, 4000);
+		}
+	}
+	
+}
+
+class Player extends Fighter{
+	constructor(canvas){
+		super(canvas);		
+		this.x = Math.floor(Globals.screenWidth / 2);
+		this.y = Globals.screenHeight - 60;
+		this.name = "player";
+		this.picPath = "greenHonda/honda_w1.png";
+		this.picRightPath = "greenHonda/honda_w2.png";
+		this.picLeftPath = "greenHonda/honda_w2L.png";
+		this.punchPicPath = "greenHonda/honda_punch.png";
+		this.punchLeftPicPath = "greenHonda/honda_punchL.png";
+	}
+}
+
+class Enemy extends Fighter{
+	constructor(canvas){
+		super(canvas);		
+		this.x = 10;		
+		this.name = "enemy";
+		this.picPath = "blee.png";
+		this.picRightPath = "blee.png";
+		this.picLeftPath = "bleeL.png";
+		this.punchPicPath = "bleePunch.png";
+		this.punchLeftPicPath = "bleeL.png";
+		this.direction = Direction.RIGHT;
+	}
+	
+	move(){
+		if (this.direction == Direction.LEFT){
+			if (this.x < 10){
+				this.direction = Direction.RIGHT;
+			}
+			else{
+				this.moveLeft();
+			}
+		}
+		else {
+			if (this.x > Globals.screenWidth - 100){
+				this.direction = Direction.LEFT;
+			}
+			else{
+				this.moveRight();
+			}
+		}
+		const context = C64Blackbox.texture.image.getContext('2d');
+		context.fillStyle = C64Blackbox.backgroundColor;
+        context.fillRect(0, Math.floor(5 * Globals.screenHeight / 6), C64Blackbox.texture.image.width, C64Blackbox.texture.image.height);
+	}
+}
+
+class Game{
+	
+	static hitDistance = 50;
+	
+	constructor(canvas){
+		this.canvas = canvas;
+		this.reset();
+	}
+	
+	activate(){
+		this.reset();
+		this.active = true;
+		console.log("Game started.");
+		this.draw();
+		this.startMainLoop();
+	}
+	
+    startMainLoop() {
+        if (this.active) {
+            this.mainLoop();
+            setInterval(() => {
+                if (this.active) {
+                    this.mainLoop();
+                }
+            }, 30);
+        }
+    }
+
+    mainLoop() {
+        this.enemy.move();
+		let accidentalPunch = Math.random() < 0.009;
+			
+		if (accidentalPunch){
+			this.punch(this.enemy);
+		}
+        this.draw();
+    }
+	
+	moveFighterLeft(fighter){
+		fighter.moveLeft();
+		this.draw();
+	}
+	
+	moveFighterRight(fighter){
+		fighter.moveRight();
+		this.draw();		
+	}
+	
+		
+	punch(fighter){
+		var hit = false;
+		if (!fighter.punching){
+			fighter.punch();
+		}		
+			this.draw();
+			var anotherFighter = this.getFighters().filter(f => f !== fighter)[0]
+			if (this.checkHitDistance(fighter, anotherFighter)){
+				if (anotherFighter.hp > 0){
+					anotherFighter.hp--;
+					anotherFighter.moveBack(17);
+				}
+				else {
+					anotherFighter.checkIfDead();
+				}
+			}
+	}
+	
+	checkHitDistance(attackingFighter, receivingFighter){
+		var distance = Math.abs(attackingFighter.x - receivingFighter.x);
+		
+		if (attackingFighter instanceof Player && receivingFighter instanceof Enemy) {
+			console.log("Attacker = Player. Enemy under attack. Distance =" + distance);
+		}
+		else{
+			console.log("Attacker = Enemy. Player under attack.. Distance =" + distance);
+		}
+		
+		return distance < Game.hitDistance;
+	}
+	
+	reset(){
+		this.active = false;
+		this.player = new Player(this.canvas);
+		this.enemy = new Enemy(this.canvas);
+		
+		console.log("Game reset.");
+	}
+	
+	draw(){		
+		this.player.draw();
+		this.enemy.draw();
+	}
+	
+	getFighters(){
+		return [this.player, this.enemy];
+	}
+}
+
+class PunchAudio {
+    constructor(audioFile) {
+        this.audio = new Audio(audioFile);
+        this.audio.preload = 'auto';
+		this.playback = false;
+    }
+	
+	enable(){
+		this.playback = true;
+	}
+
+    playAudio() {
+        this.audio.play();
+		this.playback = false;
+    }
+}
