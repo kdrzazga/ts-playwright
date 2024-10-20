cdf2c537d670649c6d8d6a9a139efab3ec492ef2
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-10-20 20:40:43 +0200

    dizzol1 - refactor

--- other/c64blackbox/minigame.js
+++ other/c64blackbox/minigame.js
@@ -1,36 +1,3 @@
-
-class PictureLoader{
-	
-	constructor(context){
-		this.context = context;
-		this.textureLoader = new THREE.TextureLoader();
-	}
-	
-	load(fileName, x, y) {
-	
-		this.textureLoader.load(fileName, (texture) => {
-			
-			const tmpCanvas = document.createElement('canvas');
-			const tmpCtx = tmpCanvas.getContext('2d');
-			
-			tmpCanvas.width = this.context.canvas.width;
-			tmpCanvas.height = this.context.canvas.height;
-			
-			tmpCtx.drawImage(this.context.canvas, 0, 0);
-			
-			tmpCtx.drawImage(texture.image, x, y);
-			
-			this.context.clearRect(0, 0, this.context.canvas.width, this.context.canvas.height);
-			this.context.drawImage(tmpCanvas, 0, 0);
-			
-			//console.log('Picture loaded and displayed at', x, y);
-		}, undefined, (error) => {
-			console.error('An error occurred while loading the texture:', error);
-		});
-	}
-	
-}
-
 class Fighter{
 	constructor(canvas){
 		this.speed = 3;
