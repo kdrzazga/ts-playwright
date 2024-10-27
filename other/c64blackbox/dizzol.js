904e201871747ffcb68ee9b41b7695b897e2fdb1
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-10-27 18:20:41 +0100

    each room has its own picture loader to load the background

--- other/c64blackbox/dizzol.js
+++ other/c64blackbox/dizzol.js
@@ -11,10 +11,13 @@ class Dizzy extends Sprite{
 
 class Room{
 
-    constructor(number, picPath){
+    constructor(number, canvas, picPath){
         this.number = number;
         this.picPath = picPath;
         this.checkpoints = [];
+
+        let context = canvas.getContext('2d');
+        this.loader = new PictureLoader(context);
     }
 
     addCheckpoint(x, y){
@@ -27,11 +30,10 @@ class DizzolGame{
     constructor(canvas){
         this.canvas = canvas;
         let roomReg = new RoomRegistry();
-        this.rooms = roomReg.createRoomSet();
+        this.rooms = roomReg.createRoomSet(canvas);
         this.player = new Dizzy(canvas);
 
-        let context = this.canvas.getContext('2d');
-        this.bkgndPicLoader = new PictureLoader(context);
+        let context = canvas.getContext('2d');
         this.dizzyPicLoader = new PictureLoader(context);
         this.reset();
     }
@@ -47,13 +49,14 @@ class DizzolGame{
 		console.log("Dizzol Game started.");
 
         let currentRoom = this.rooms.find(room => room.number === this.currentRoomId);
-        console.log(currentRoom.number + " " + currentRoom.picPath);
-        this.bkgndPicLoader.load(currentRoom.picPath, 0, 9 * C64Blackbox.rowHeight);
+        console.log("Total rooms = " + this.rooms.length + " Current room =" + currentRoom.number + " " + currentRoom.picPath);
+        currentRoom.loader.load(currentRoom.picPath, 0, 9 * C64Blackbox.rowHeight);
         this.dizzyPicLoader.load(this.player.picPath, 289, 20.5 * C64Blackbox.rowHeight);
 	}
 
     draw(){
-        this.bkgndPicLoader.draw(0, 9 * C64Blackbox.rowHeight);
+        let currentRoom = this.rooms.find(room => room.number === this.currentRoomId);
+        currentRoom.loader.draw(0, 9 * C64Blackbox.rowHeight);
         this.dizzyPicLoader.draw(this.player.x , 20.5 * C64Blackbox.rowHeight);
     }
 
@@ -73,10 +76,10 @@ class DizzolGame{
 
 class RoomRegistry{
 
-    createRoomSet(){
-        const room1 = new Room(1, "dizzol/1.png");
+    createRoomSet(canvas){
+        const room1 = new Room(1, canvas, "dizzol/1.png");
         room1.addCheckpoint(10, 300);
-        const room2 = new Room(2, "dizzol/2.png");
+        const room2 = new Room(2, canvas, "dizzol/2.png");
 
         return [room1, room2];
     }
