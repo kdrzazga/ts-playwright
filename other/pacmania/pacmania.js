f2e57ad9c98599cbda8e430727670e1304082c94
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-11-07 09:56:31 +0100

    pacmania2 - movement

--- other/pacmania/pacmania.js
+++ other/pacmania/pacmania.js
@@ -19,13 +19,33 @@ class Maze {
         this.mesh.rotation.x = -Math.PI / 2;
         scene.add(this.mesh);
     }
+
+    move(direction) {
+        const movementSpeed = 0.02;
+        switch (direction) {
+            case 'UP':
+                this.mesh.position.z += movementSpeed;
+                break;
+            case 'DOWN':
+                this.mesh.position.z -= movementSpeed;
+                break;
+            case 'LEFT':
+                this.mesh.position.x += movementSpeed;
+                break;
+            case 'RIGHT':
+                this.mesh.position.x -= movementSpeed;
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 class Game {
     constructor() {
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        this.camera.position.set(5, 5, 10);
+        this.camera.position.set(3, 2, 7.5);
         this.camera.lookAt(0, 0, 0);
         this.renderer = new THREE.WebGLRenderer();
         this.renderer.setSize(window.innerWidth, window.innerHeight);
@@ -40,7 +60,25 @@ class Game {
             this.renderer.setSize(window.innerWidth, window.innerHeight);
         });
 
+        window.addEventListener('keydown', (event) => this.handleKeyDown(event));
+
         this.animate();
+    }
+
+    handleKeyDown(event){
+        if ('w' === event.key || 'ArrowUp' === event.key) {
+            console.log('UP');
+            this.maze.move('UP');
+        } else if ('s' === event.key || 'ArrowDown' === event.key) {
+            console.log('DOWN');
+            this.maze.move('DOWN');
+        } else if ('a' === event.key || 'ArrowLeft' === event.key) {
+            console.log('LEFT');
+            this.maze.move('LEFT');
+        } else if ('d' === event.key || 'ArrowRight' === event.key) {
+            console.log('RIGHT');
+            this.maze.move('RIGHT');
+        }
     }
 
     animate() {
