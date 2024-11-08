0fe3ff36a22b62de13358607b69e9b9ee9ea9403
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-11-08 22:17:41 +0100

    Pacman in a forest: Mushroom

--- other/pacmania/board.js
+++ other/pacmania/board.js
@@ -15,18 +15,17 @@ class MovableObject {
     }
 }
 
-class Tree extends MovableObject{
-    constructor(x, z) {
+class Plant extends MovableObject{
+    constructor(x, z, filename, height) {
         super();
 
         const width = 1;
-        const height = 2;
-        const depth = 0.1;
+        const depth = 0.03;
 
         const geometry = new THREE.BoxGeometry(width, height, depth);
 
         const textureLoader = new THREE.TextureLoader();
-        const texture = textureLoader.load('resources/tree.png');
+        const texture = textureLoader.load(filename);
         const material = new THREE.MeshBasicMaterial({ map: texture });
 
         this.mesh = new THREE.Mesh(geometry, material);
@@ -34,10 +33,22 @@ class Tree extends MovableObject{
     }
 }
 
-class Board  extends MovableObject{
+class Tree extends Plant{
+    constructor(x, z){
+        super(x, z, 'resources/tree.png', 2);
+    }
+}
+
+class Mushroom extends Plant{
+    constructor(x, z){
+        super(x, z, 'resources/mushroom.png', 0.7);
+    }
+}
+
+class Board extends MovableObject{
     constructor(scene) {
         super();
-        this.geometry = new THREE.PlaneGeometry(10, 40);
+        this.geometry = new THREE.PlaneGeometry(12, 40);
         this.material = new THREE.MeshBasicMaterial({
             color: 0x00aa00,
             side: THREE.DoubleSide,
@@ -51,17 +62,27 @@ class Board  extends MovableObject{
         this.mesh.rotation.x = -Math.PI / 2;
 
         this.trees = [];
+        this.mushrooms = [];
 
-        const treePositions = [[1, 0], [-3, 3], [4.2, 7.5], [-1.9, 7.5], [3.1, 5.5], [-2.9, -1.5], [-1.1, -.5]];
+        const treePositions = [[1, 0], [-3, 3], [4.2, 7.5], [-1.9, 7.5], [3.1, 5.5], [-2.9, -1.5], [-1.1, -.5]
+            , [-1.1, -19.5], [3, -15.5], [4.9, -14.5], [-4.9, 14.5]];
+
+        const mushroomPositions = [[1.5, -13], [2, 11], [-4, 5], [5.2, 3.5], [-3.9, 14.35]];
 
         treePositions.forEach(point =>{
             let t = new Tree(point[0], point[1]);
             this.trees.push(t);
         });
 
+        mushroomPositions.forEach(point =>{
+            let m = new Mushroom(point[0], point[1]);
+            this.mushrooms.push(m);
+        });
+
         scene.add(this.mesh);
 
         this.trees.forEach(t => scene.add(t.mesh));
+        this.mushrooms.forEach(m => scene.add(m.mesh));
     }
 
     createGridTexture() {
@@ -109,7 +130,8 @@ class Board  extends MovableObject{
     //@Override
     move(deltaX, deltaZ) {
         super.move(deltaX, deltaZ);
-        this.trees.forEach( t => t.move(deltaX, deltaZ));
+        this.trees.forEach(t => t.move(deltaX, deltaZ));
+        this.mushrooms.forEach(m => m.move(deltaX, deltaZ));
     }
 
 }
