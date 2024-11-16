46bf3468f18eba9aee6cfdc6736cd07e0ced8e52
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-11-16 16:07:09 +0100

    small fixes + Arkanoid 1

---
+++ other/arkanoid/arkanoid.js
@@ -0,0 +1,70 @@
+class Velocity {
+    constructor(speed) {
+        this.speed = speed;
+        this.directions = [Math.PI / 4, 3 * Math.PI / 4, -3 * Math.PI / 4, -Math.PI / 4];
+        this.currentIndex = 0;
+    }
+
+    deflectOnLeft() {
+        this.currentIndex = (this.currentIndex + 1) % 4;
+        this.angle = this.directions[this.currentIndex];
+        return this;
+    }
+
+    deflectOnRight() {
+        this.currentIndex = (this.currentIndex + 3) % 4; // Equivalent to -1 index rotation
+        this.angle = this.directions[this.currentIndex];
+        return this;
+    }
+
+    deflectOnTop() {
+        this.currentIndex = 0;
+        this.angle = this.directions[this.currentIndex];
+        return this;
+    }
+
+    deflectOnBottom() {
+        this.currentIndex = 2;
+        this.angle = this.directions[this.currentIndex];
+        return this;
+    }
+
+    getVelocityVector() {
+        const x = this.speed * Math.cos(this.angle);
+        const y = this.speed * Math.sin(this.angle);
+        return { x, y };
+    }
+}
+
+class Ball{
+
+    constructor(){
+        #TODO
+    }
+}
+
+class Bat{
+
+    constructor(){
+        #TODO
+    }
+}
+
+class Brick{
+
+    static WIDTH = 30;
+    static HEIGHT = 10;
+
+    constructor(color){
+        this.color = color;
+    }
+}
+
+class Board{
+
+    constructor(){
+        this.bricks =[new Brick()];
+        this.bat = new Bat();
+        this.ball = new Ball();
+    }
+}
