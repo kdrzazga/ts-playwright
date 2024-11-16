519889017e230558d05ba5774bdeb1b5a1855324
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-11-16 05:56:49 +0100

    Brainfuck 1 (#68)

---
+++ other/brainfuck/debugger.js
@@ -0,0 +1,144 @@
+class BrainfuckDebugger{
+
+    constructor(program){
+        this.program = program;
+        this.instructionPointer = 0;
+
+        this.memory = [0];
+        this.memoryPointer = 0;
+    }
+
+    validateCode(){
+        let program = this.program;
+        const brainfuckKeywords = ['+', '-', '>', '<', '.'];
+        brainfuckKeywords.forEach(keyword => program.replaceAll('\\' + keyword, ''));
+        if (program.length < 0){
+            const message = 'Error. Invalid keywords.';
+            console.error(message);
+            window.alert(message);
+        }
+        return program.length < 0;
+    }
+
+    step(){
+        if (this.instructionPointer >= this.program.length){
+            window.alert('End of program.');
+            return;
+        }
+
+        this.instructionPointer++;
+        const command = this.getCurrentCommand(); ;
+        if ('' === command)
+            return;
+
+        else if('+' === command){
+            let value = this.memory[this.memoryPointer];
+            value++;
+            if (value>255)
+                value = 256 - value;
+            this.memory[this.memoryPointer] = value;
+        }
+
+        else if('-' === command){
+            let value = this.memory[this.memoryPointer];
+            value--;
+            if(value < 0)
+                value = 256 + value;
+
+            this.memory[this.memoryPointer] = value;
+        }
+
+        else if('<' === command){
+            if(this.memoryPointer > 0){
+                this.memoryPointer--;
+            }
+            else{
+                console.warn('Cannot go lower than Memory Cell 0');
+            }
+        }
+
+        else if('>' === command){
+            const initialValue = 0;
+
+            if (this.memoryPointer >= this.memory.length - 1) {
+                this.memory.push(initialValue);
+            }
+
+            this.memoryPointer++;
+        }
+
+        else if('.' === command){
+            const outputTextarea = document.getElementById('output');
+            let oldValue = outputTextarea.value;
+            outputTextarea.value = '';
+            this.memory.forEach(cell =>{
+                let newValue = oldValue + String.fromCharCode(cell);
+                outputTextarea.value = newValue;
+            });
+        }
+    }
+
+    getCurrentCommand(){
+        if(this.instructionPointer < this.program.length){
+            return this.program.charAt(this.instructionPointer);
+        }
+        else{
+            console.warn("End of program.");
+            return "";
+        }
+    }
+
+    getMemory(){
+        return this.memory;
+    }
+}
+
+function readProgram(){
+    const programTextArea = document.getElementById('program');
+    let programTextAreaCode = programTextArea.value;
+    let code = programTextAreaCode.replace(/\s+/g, '');
+    const bfDebugger = new BrainfuckDebugger(code);
+    bfDebugger.getCurrentCommand();
+    return bfDebugger;
+}
+
+function updateLabels(){
+    if (null == bfDebugger)
+        return;
+
+    const pointerLabel = document.getElementById('instruction-pointer');
+    const commandLabel = document.getElementById('command');
+
+    pointerLabel.innerText = "Instruction Pointer: " + bfDebugger.instructionPointer;
+    commandLabel.innerText = "Command: " + bfDebugger.getCurrentCommand();
+}
+
+function updateMemoryTable(){
+    if (null == bfDebugger)
+        return;
+
+    let memoryTableHeader = document.getElementById('cell-number');
+    let memoryTableValues = document.getElementById('cell-value');
+
+    const rows = [memoryTableHeader, memoryTableValues];
+
+    rows.forEach(row =>{
+         while (row.cells.length > 0){
+            row.deleteCell(0);
+         }
+    });
+
+    const mem = bfDebugger.getMemory();
+
+    for (var i = 0; i < mem.length; i++){
+        let headerCell = memoryTableHeader.insertCell();
+        headerCell.textContent = i;
+
+        let valueCell = memoryTableValues.insertCell();
+        valueCell.textContent = mem[i];
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    let bfDebugger = null;
+});
---
+++ other/brainfuck/programs.js
@@ -0,0 +1,61 @@
+class ProgramObjectPool{
+
+    constructor(){
+      this.programs = {
+        '0':{
+            'description': 'Hello Brainfuck',
+            'code': '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.>+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.'
+                 + '>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.'
+                 + '>++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.'
+                 + '>+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++.'
+            },
+        '1':{
+            'description': '0 255 254 253 252',
+            'code': '>->-->--->---->-----'
+            },
+        '2':{
+            'description': '123',
+            'code': '+>++>+++>'
+            }
+      }
+    }
+
+    getProgram(index){
+        let correctIndex = index % this.programs.length;
+        return this.programs[correctIndex];
+    }
+
+}
+
+function populatePredefinedProgramsSelect(){
+
+    const pool = new ProgramObjectPool();
+    const keys = Object.keys(pool.programs);
+
+    let predefinedProgramsSelect = document.getElementById('predefined-programs');
+    var i = 1;
+
+    keys.forEach(k => {
+        var option = document.createElement("option");
+        option.value = pool.programs[k].description;
+        option.textContent = pool.programs[k].description;
+        predefinedProgramsSelect.appendChild(option);
+    });
+}
+
+function loadPredefinedProgram(){
+    const predefinedProgramsSelect = document.getElementById('predefined-programs');
+    let programToBeLoadedIndex = predefinedProgramsSelect.selectedIndex;
+    let programToBeLoaded = predefinedProgramsSelect.selectedOptions[0].text;
+
+    console.log(`Loading ${programToBeLoadedIndex}. ${programToBeLoaded}`);
+
+    const pool = new ProgramObjectPool();
+    const programTextArea = document.getElementById('program');
+    programTextArea.value = '';
+    programTextArea.value = pool.programs[programToBeLoadedIndex].code;
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    populatePredefinedProgramsSelect();
+});
