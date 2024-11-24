95c086bb56b080370a8c796d22f71058f72d8ec6
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-11-24 21:13:17 +0100

    restored room

--- other/c64blackbox/dizzol.js
+++ other/c64blackbox/dizzol.js
@@ -109,6 +109,132 @@ class Knight extends Sprite{
 
 }
 
+class Room{
+
+    constructor(number, canvas, picPath, leftExit, rightExit, floorLevels, checkpoints, batsCount){
+        this.number = number;
+        this.picPath = picPath;
+        this.leftExit = leftExit;
+        this.rightExit = rightExit;
+		this.floorLevels = floorLevels;
+		this.checkpoints = checkpoints;
+
+        this.context = canvas.getContext('2d');
+        this.loader = new PictureLoader(this.context);
+        this.enemyLoader = new PictureLoader(this.context);
+        this.garlicLoader = new PictureLoader(this.context);
+        this.bats = [];
+        this.items = [];
+        this.info = '';
+
+        this.initializeBats(batsCount, canvas);
+    }
+
+    initializeBats(batsCount, canvas){
+        for (let i = 0; i < batsCount; i++) {
+            const bat = new Bat(canvas, 291 + 39 * i, 2 + i);
+            this.bats.push(bat);
+        }
+        if (this.bats.length > 0) {
+            this.bats[0].x = 326;
+        }
+        console.log('Created bats in Room ' + this.number);
+    }
+
+    load(){
+        this.read();
+        this.draw();
+    }
+
+    read() {
+        this.loadPicture(this.loader, this.picPath).catch(error => {
+            console.error('Failed to load picture:', error);
+        });
+
+        if (this.bats.length > 0) {
+            this.loadPicture(this.enemyLoader, this.bats[0].picPath).catch(error => {
+                console.error('Failed to load bat picture:', error);
+            });
+        }
+
+        this.loadPicture(this.garlicLoader, Garlic.PATH).catch(error => {
+            console.error('Failed to load garlic picture:', error);
+        });
+    }
+
+    loadPicture(loader, path) {
+        if (path) {
+            loader.fileName = path;
+            return loader.read().then(texture => {
+                loader.texture = texture;
+            });
+        }
+        return Promise.resolve();  // No picture to load
+    }
+
+    draw(){
+        this.loader.draw(0, 9 * C64Blackbox.rowHeight);
+        //this.writeRoomInfo();
+    }
+
+    writeRoomInfo(){
+        this.writeUpperInfo(this.info);
+    }
+
+    writeUpperInfo(text){
+        const rowHeight = C64Blackbox.rowHeight;
+        const cursor = this.c64Blackbox.cursor;
+        const y = (2 + 3 * 2) * rowHeight;
+        const x = 2*rowHeight;
+        this.context.fillStyle = cursor.backgroundColor;
+        this.context.fillRect(x - 2, y - rowHeight + 2, 39*cursor.size + 4, cursor.size + 8);
+        this.context.fillStyle = cursor.color;
+        this.context.fillText(text, x, y);
+    }
+
+    drawEnemies(){
+       this.bats.filter(enemy => enemy.hp>0).forEach(bat => this.enemyLoader.draw(bat.x, bat.y));
+    }
+
+    drawItems(){
+        let garlics = this.items.filter(i => 'garlic' === i.name);
+        garlics.forEach(item => this.garlicLoader.draw(item.x, item.y));
+    }
+
+    animate(){
+        if (this.bats.length > 0){
+
+            this.bats.forEach(b => b.move());
+            this.draw();
+            this.drawItems();
+            this.drawEnemies();
+        }
+    }
+
+	getFloorLevel(x) {
+        for (let { range, level } of this.floorLevels) {
+            if (x >= range[0] && x <= range[1]) {
+                return level;
+            }
+        }
+        return null;
+    }
+
+    addItemOnFloor(item){
+        let x = item.x;
+        item.y = this.getFloorLevel(x) + 27;
+        this.items.push(item);
+    }
+
+    setInfo(info){
+        this.info = info;
+    }
+
+    setC64Blackbox(c64Blackbox){
+        this.c64Blackbox = c64Blackbox;
+    }
+}
+
 class DizzolGame{
 
     static ROOM1 = 1;
