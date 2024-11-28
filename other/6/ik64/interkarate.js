edfbf82c65379f98833c06d980859134be20ab52
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-11-28 18:01:27 +0100

    JS -> TS 7 IK+

--- other/6/ik64/interkarate.js
+++ other/6/ik64/interkarate.js
@@ -1,24 +1,23 @@
-class Commodore64 {
-  static FPS = 4;
-  static BLUE = "#352879";
-  static LIGHTBLUE = "#6c5eb5";
-
-  private tableContentHeader: string[];
-  private tableContent: string[];
-  private blink = true;
-  private lastRenderTime;
-  private canvas;
-  private canvasContainer;
-  private frameIndex = 0;
-  private frames = ['kw1', 'kw2', 'kw3'];
-  private karatekaCell;
-  private direction = -1;
-  private stepCounter = 0;
-  private stepMax = 12 +  Math.floor(Math.random() * 38);
+class InterKarate64 extends Commodore64{
 
   constructor() {
+      super();
+
+      this.tableContentHeader;
+      this.tableContent;
+      this.blink = true;
+      this.lastRenderTime;
+      this.canvas;
+      this.canvasContainer;
+      this.frameIndex = 0;
+      this.frames = ['kw1', 'kw2', 'kw3'];
+      this.karatekaCell;
+      this.direction = -1;
+      this.stepCounter = 0;
+      this.stepMax = 12 +  Math.floor(Math.random() * 38);
+
     this.tableContentHeader = [
-      "&nbsp",		
+      "&nbsp",
       "<center>    &nbsp**** COMMODORE 64 BASIC V2 ****&nbsp    </center>",
       "&nbsp",
       "<center> &nbsp64K RAM SYSTEM  38911 BASIC BYTES FREE&nbsp </center>"
@@ -49,18 +48,19 @@ class Commodore64 {
 	this.lastRenderTime = 0;
   }
 
-  generateHtml(): string {
+  //override
+  generateHtml() {
     const html = [];
 	let number = 0;
     html.push("<table id=\"main\" bgcolor=\"" + Commodore64.BLUE + "\">");
-	
+
     this.tableContentHeader.forEach((line) => {
 		let strNumber = String(number);
 		let style = line === "&nbsp" ? "" : "style='white-space: nowrap;'";
 		html.push("<tr><td id=\"row" + strNumber + "\" colspan=\"2\" " + style + ">" + line + "</td></tr>");
 		number++;
 	});
-	
+
     this.tableContent.forEach((line) => {
 	  let strNumber = String(number);
       html.push("<tr><td id=\"row" + strNumber + "col1\">" + line + "</td><td id=\"row" + strNumber + "col2\"></td></tr>");
@@ -69,17 +69,7 @@ class Commodore64 {
     html.push("</table>");
     return html.join("");
   }
-  
-  generateBorder(): string {
-    const html = [];
-	html.push("<table bgcolor=\"" + Commodore64.LIGHTBLUE + "\" width=\"100%\">")
-	for (let i = 0; i < 3; i++){
-		html.push("<tr><td width=\"10%\">&nbsp</td><td width=\"80%\">&nbsp</td><td width=\"10%\">&nbsp</td></tr>");
-	}
-	html.push("</table>");
-    return html.join("");  
-  }
-  
+
   initAnim(){
 	this.karatekaCell = document.getElementById('row5col2');
 	this.karatekaCell.innerHTML = "<img src = \"resources/k.png\"/>";
@@ -120,7 +110,7 @@ let height = 600;
 window.innerWidth = width;
 window.innerHeight = height;
 
-const commodore64 = new Commodore64();
+const commodore64 = new InterKarate64();
 const html = commodore64.generateHtml();
 const div = document.getElementById('commodore64');
 const topBorderDiv = document.getElementById('top-border');
