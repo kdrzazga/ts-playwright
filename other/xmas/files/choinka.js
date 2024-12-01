58edcfee0bdc26f2661c499c9c7c5212b3e031a9
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-12-01 14:32:07 +0100

    XMas 14 Scroller class

--- other/xmas/files/choinka.js
+++ other/xmas/files/choinka.js
@@ -2,21 +2,6 @@ class Petscii{
     static TRIANGLE_LEFT = 0;
 }
 
-const step = window.innerHeight / 300;
-let y = 0;
-let direction = 1;
-
-function smoothScroll() {
-    y += step * direction;
-    
-    if (y >= document.body.scrollHeight - window.innerHeight || y <= 0) {
-        direction *= -1;
-    }
-    
-    window.scrollTo(0, y);
-    requestAnimationFrame(smoothScroll);
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     let spans = document.querySelectorAll('span[name="slash"]');
     let backslashSpans = document.querySelectorAll('span[name="backslash"]');
@@ -27,6 +12,8 @@ document.addEventListener('DOMContentLoaded', function() {
         span.innerText = String.fromCharCode(0x25e5);
     });
 
+    const scroller = new Scroller(-1);
+    scroller.startScrolling();
+
     console.log("Merry Christmas !");
-    smoothScroll();
 });
