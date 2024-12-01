c387d6464a1958f97a737f9df343963d7327ac81
kdrzazga <krzysztof.drzazga@tutanota.com>
2024-12-01 07:59:13 +0100

    XMas 6 santa3

---
+++ other/xmas/files/xmas.js
@@ -0,0 +1,36 @@
+var counter = 3;
+const counterMax = 3;
+
+function moveSanta1() {
+    var santa1 = document.getElementById('santa1');
+    santa1.style.left = (parseInt(santa1.style.left) + 6) + 'px';
+    santa1.style.top = (parseInt(santa1.style.top) + 1) + 'px';
+}
+function conditionalSanta1Reset() {
+    var santa1 = document.getElementById('santa1');
+    if (parseInt(santa1.style.left) > 3000) {
+        santa1.style.left = '-300px';
+        santa1.style.top = '200px';
+    }
+}
+
+function timeLoop() {
+    var _this = this;
+    var lastRenderTime = 0;
+    var currentTime = performance.now();
+    var timeSinceLastRender = currentTime - lastRenderTime;
+    if (timeSinceLastRender >= 15) {
+        if (counter < 0) {
+            moveSanta1();
+            conditionalSanta1Reset();
+            counter = counterMax;
+            lastRenderTime = currentTime;
+        }
+        else {
+            counter--;
+        }
+    }
+    requestAnimationFrame(function () { return _this.timeLoop(); });
+}
+
+timeLoop();
